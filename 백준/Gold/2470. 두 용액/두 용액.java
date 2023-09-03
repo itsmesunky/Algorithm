99d032825d5e0b868212b3281885d8520e34fdda
Sunky <99164690+itsmesunky@users.noreply.github.com>
2023-09-03 20:25:13 +0900

    [Gold V] Title: 두 용액, Time: 368 ms, Memory: 28464 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -16,31 +16,23 @@ public class Main {
 		
 		Arrays.sort(arr);
 		
-		if(arr[0] >= 0) {
-			System.out.println(arr[0] + " " +arr[1]);
-		} else {
-			int min = Integer.MAX_VALUE;
-			int p1 = 0, p2 = n -1, num1 = 0, num2 = 0, sum = 0, tmp =0;
-			while(p1 < p2) {
-				sum = arr[p1] + arr[p2];
-				if(sum == 0) {
-					System.out.println(arr[p1] + " " + arr[p2]);
-					break;
-				} else {
-					tmp = Math.abs(sum);
-					if(tmp < min) {
-						min = tmp;
-						num1 = arr[p1];
-						num2 = arr[p2];
-					}
-					if(sum < 0) {
-						p1++;
-					} else {
-						p2--;
-					}
-				}
+		int min = Integer.MAX_VALUE;
+		int p1 = 0, p2 = n -1, num1 = 0, num2 = 0, sum = 0, tmp =0;
+		while(p1 < p2) {
+			sum = arr[p1] + arr[p2];
+			tmp = Math.abs(sum);
+			if(tmp < min) {
+				min = tmp;
+				num1 = arr[p1];
+				num2 = arr[p2];
 			}
-			System.out.println(num1 + " " + num2);
+			if(sum < 0) {
+				p1++;
+			} else {
+				p2--;
+			}
 		}
+		System.out.printf("%d %d", num1, num2);
+		
 	}
 }
