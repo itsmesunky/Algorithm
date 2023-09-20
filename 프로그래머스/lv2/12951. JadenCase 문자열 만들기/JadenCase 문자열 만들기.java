bbd77483e80aa04a2ca7a1f7351478759fa6478a
Sunky <99164690+itsmesunky@users.noreply.github.com>
2023-09-20 16:10:44 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 12.24 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,14 +1,27 @@
 class Solution {
-  public String solution(String s) {
-        String answer = "";
-        String[] sp = s.toLowerCase().split("");
-        boolean flag = true;
+    public String solution(String s) {
+        // 전체 소문자로 변환 후, 공백을 기준으로 String 배열 생성
+        String[] strArr = s.toLowerCase().split(" ");
 
-        for(String ss : sp) {
-            answer += flag ? ss.toUpperCase() : ss;
-            flag = ss.equals(" ") ? true : false;
+        if(s.substring(s.length() - 1, s.length()).equals(" ")) {
+            strArr[strArr.length-1] += " ";
         }
 
+        String answer = "";
+
+        for(int i = 0; i < strArr.length; i++) {
+            for(int j = 0; j < strArr[i].length(); j++) {
+                // 첫 글자가 영문일 경우 대문자로 변환
+                if(j == 0 && Character.isAlphabetic(strArr[i].charAt(j))) {
+                    answer += Character.toUpperCase(strArr[i].charAt(j));
+                } else {
+                    answer += strArr[i].charAt(j);
+                }
+            }
+            if(i != strArr.length -1) {
+                answer += " ";
+            }
+        }
         return answer;
-  }
+    }
 }
