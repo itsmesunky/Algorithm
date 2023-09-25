7dcd900034c197128a0d6af7d58b52194e87778f
Sunky <99164690+itsmesunky@users.noreply.github.com>
2023-09-25 17:40:21 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.55 ms, Memory: 70.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Solution {
+    public String solution(String s) {
+       StringBuilder sb = new StringBuilder();
+		
+		// 소문자 List
+		List<Character> lowerCaseList = new ArrayList<Character>();
+		// 대문자 List
+		List<Character> upperCaseList = new ArrayList<Character>();
+		
+		for(char ch : s.toCharArray()) {
+			if(Character.isLowerCase(ch)) {
+				lowerCaseList.add(ch);
+			} else {
+				upperCaseList.add(ch);
+			}
+		}
+		
+		// 소문자 List 내림차순 정렬
+		lowerCaseList.sort(Collections.reverseOrder());
+		// 대문자 List 내림차순 정렬
+		upperCaseList.sort(Collections.reverseOrder());
+		
+		for(char ch : lowerCaseList) {
+			sb.append(ch);
+		}
+		
+		for(char ch : upperCaseList) {
+			sb.append(ch);
+		}
+		
+		return sb.toString();
+    }
+}
