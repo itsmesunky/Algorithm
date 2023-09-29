23586aafc75b77883f5dd44bd6b25e85d1763815
Sunky <99164690+itsmesunky@users.noreply.github.com>
2023-09-29 18:18:20 +0900

    [level 2] Title: 프로세스, Time: 0.78 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.java
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.java
@@ -1,23 +1,43 @@
 import java.util.*;
 
+// 프로세스 클래스 생성
+class Process {
+	int idx; // index
+	int priority; // 우선순위
+	
+	public Process(int idx, int priority) {
+		this.idx = idx;
+		this.priority = priority;
+	}
+}
+
 class Solution {
     public int solution(int[] priorities, int location) {
-        int answer = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-		
-		for(int num : priorities) {
-			pq.add(num);
+        // Queue 생성
+		Queue<Process> Q = new LinkedList<Process>();
+		for(int i = 0; i < priorities.length; i++) {
+			Q.offer(new Process(i, priorities[i]));
 		}
-		while(!pq.isEmpty()) {
-			for(int i=0; i<priorities.length; i++) {
-				if(priorities[i] == pq.peek()) {
-					pq.poll();
-					answer++;
-					if(i == location)
-						return answer;
+		
+		int answer = 0;
+		
+		while(!Q.isEmpty()) {
+			Process tmp = Q.poll();
+			for(Process proc : Q) {
+				if(tmp.priority < proc.priority) {
+					Q.offer(tmp);
+					tmp = null;
+					break;
 				}
 			}
-		}  
-        return answer;
+			if(tmp != null) {
+				answer++;
+				if(tmp.idx == location) {
+					return answer;
+				}
+			}
+		}
+		
+		return answer;
     }
 }
