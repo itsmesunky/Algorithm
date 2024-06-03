b490c7c4f0c536c87fcafb2d9e90cb23ddfe89eb
Sunky <99164690+itsmesunky@users.noreply.github.com>
2024-06-03 14:04:17 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,22 +1,23 @@
-SELECT  A.*
+SELECT  *
   FROM  (
-            SELECT  CAR_ID
-                 ,  CAR_TYPE
-                 ,  CASE WHEN CAR_TYPE = '세단' THEN (DAILY_FEE - (DAILY_FEE * 0.08)) * 30
-                    ELSE (DAILY_FEE - (DAILY_FEE * 0.05)) * 30
-                    END AS FEE
-              FROM  CAR_RENTAL_COMPANY_CAR
-             WHERE  CAR_TYPE IN ('세단', 'SUV')
-               AND  CAR_ID NOT IN   (
+            SELECT  A.CAR_ID
+                 ,  A.CAR_TYPE
+                 ,  30 * A.DAILY_FEE * (1 - (B.DISCOUNT_RATE * 0.01)) AS FEE
+              FROM  CAR_RENTAL_COMPANY_CAR              A
+                 ,  CAR_RENTAL_COMPANY_DISCOUNT_PLAN    B
+             WHERE  A.CAR_TYPE = B.CAR_TYPE
+               AND  DURATION_TYPE = '30일 이상'
+               AND  A.CAR_TYPE IN ('세단', 'SUV')
+               AND  A.CAR_ID NOT IN (
                                         SELECT  A.CAR_ID
                                           FROM  CAR_RENTAL_COMPANY_CAR              A
                                              ,  CAR_RENTAL_COMPANY_RENTAL_HISTORY   B
                                          WHERE  A.CAR_ID = B.CAR_ID
                                            AND  A.CAR_TYPE IN ('세단', 'SUV')
                                            AND  TO_CHAR(B.END_DATE, 'YYYYMMDD') >= '20221101'
-                                           AND  TO_CHAR(B.START_DATE, 'YYYYMMDD') <= '20221201'
+                                           AND  TO_CHAR(B.START_DATE, 'YYYYMMDD') < '20221201'
                                     )
-        ) A
+        )   A
  WHERE  A.FEE BETWEEN 500000 AND 1999999
  ORDER  BY  FEE DESC
      ,  CAR_TYPE
