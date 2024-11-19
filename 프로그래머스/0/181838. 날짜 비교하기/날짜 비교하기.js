6389775553cc8e359cf08449bfc7a613fce8fd51
선우 <99164690+itsmesunky@users.noreply.github.com>
2024-11-19 15:12:19 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,23 +1,9 @@
+function formatDate(date) {
+    return date.map((value, index) => 
+        String(value).padStart(index === 0 ? 4 : 2, "0")
+    ).join('');
+}
+
 function solution(date1, date2) {
-    for(let i = 0; i < date1.length; i++) {
-        if(i === 0) {
-            date1[i] = String(date1[i]).padStart(4, "0");    
-        } else {
-            date1[i] = String(date1[i]).padStart(2, "0");    
-        }
-    }
-    
-    for(let i = 0; i < date2.length; i++) {
-        if(i === 0) {
-            date2[i] = String(date2[i]).padStart(4, "0");    
-        } else {
-            date2[i] = String(date2[i]).padStart(2, "0");    
-        }
-    }
-    
-    if(date1.join('') < date2.join('')) {
-        return 1;
-    } else {
-        return 0;
-    }
+    return formatDate(date1) < formatDate(date2) ? 1 : 0;
 }
