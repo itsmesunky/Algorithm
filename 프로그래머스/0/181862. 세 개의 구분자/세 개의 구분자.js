51ca1d31eceb0542da4bb4003cf8ed9c68747d2d
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-05-02 16:46:40 +0900

    [level 0] Title: 세 개의 구분자, Time: 27.91 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,28 +1,4 @@
 function solution(str) {
-    let element = "";
-    let answer = [];
-    
-    for(const char of str) {
-        switch(char) {
-            case "a": case "b": case "c":
-                if(element) {
-                answer.push(element);
-                    element = "";
-                }
-                break;
-            default:
-                element += char;
-                break;
-        }
-    }
-    
-    if(element) {
-        answer.push(element);
-    }
-    
-    if(!answer.length) {
-        answer.push("EMPTY");
-    }
-    
-    return answer;
+    const answer = str.split(/[abc]/).filter(item => item.length !== 0);
+    return answer.length === 0 ? ["EMPTY"] : answer; 
 }
