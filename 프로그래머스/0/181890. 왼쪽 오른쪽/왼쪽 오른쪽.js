16c664b0d5c13ad07a6961567ef453f47f48ce85
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-05-07 17:17:42 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -1,31 +1,13 @@
 const solution = (str_list) => {
-    const strList = str_list.join('');
-    
-    let lIdx = strList.indexOf('l');
-    let rIdx = strList.indexOf('r');
-    
-    // 'l', 'r' 둘 다 없는 경우
-    if(lIdx === -1 && rIdx === -1) {
-        return [];
+    for(let i = 0; i < str_list.length; i++) {
+        if(str_list[i] === 'l') {
+            return str_list.slice(0, i);
+        }
+        
+        if(str_list[i] === 'r') {
+            return str_list.slice(i + 1);
+        }
     }
     
-    // 'l'과 'r' 둘 다 있고, 'l'이 'r'보다 먼저 나오는 경우
-    if((lIdx !== -1 && rIdx !== -1) && lIdx < rIdx) {
-        return str_list.slice(0, lIdx);
-    }
-    
-    // 'l'만 있는 경우
-    if(rIdx === -1) {
-        return str_list.slice(0, lIdx);
-    }
-    
-    // 'r'이 'l'보다 먼저 나오는 경우
-    if(lIdx !== -1 && rIdx < lIdx) {
-        return str_list.slice(rIdx + 1, str_list.length);
-    }
-    
-    // 'r'만 있는 경우
-    if(lIdx === -1) {
-        return str_list.slice(rIdx + 1, str_list.length);
-    }
+    return [];
 }
