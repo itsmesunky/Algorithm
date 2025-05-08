95d3db6050972df1afa397aa8c0b8bb6481dbd65
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-05-08 16:47:52 +0900

    [level 0] Title: 소인수분해, Time: 0.41 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.js
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.js
@@ -1,15 +1,16 @@
-function solution(n) {
-    const result = new Set();
-    let divisor = 2;
-
-    while (n > 1) {
-        if (n % divisor === 0) {
-            result.add(divisor);
-            n = n / divisor;
+const solution = (n) => {
+    const answer = [];
+    
+    let i = 2;
+    
+    while(n > 1) {
+        if(n % i === 0) {
+            answer.push(i);
+            n /= i;
         } else {
-            divisor++;
+            i++;
         }
     }
-
-    return [...result].sort((a, b) => a - b);
+    
+    return [...new Set(answer)];
 }
