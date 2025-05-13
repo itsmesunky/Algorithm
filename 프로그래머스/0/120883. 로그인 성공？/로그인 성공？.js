3bb8bea0e5312190fcc909262fec7ac6c1406d3c
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-05-13 23:16:53 +0900

    [level 0] Title: 로그인 성공?, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,9 +1,9 @@
 const solution = (id_pw, db) => {
-    let result = db.filter((account) => account[0] === id_pw[0]);
+    let result = db.find((account) => account[0] === id_pw[0]);
     
-    if(!result.length) {
+    if(!result) {
         return "fail";
     } else {
-        return result[0][1] === id_pw[1] ? "login" : "wrong pw";
+        return result[1] === id_pw[1] ? "login" : "wrong pw";
     }
 }
