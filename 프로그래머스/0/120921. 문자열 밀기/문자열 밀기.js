e7e098564e46df0e867e02be67f7b2c749facb97
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-05-15 16:18:31 +0900

    [level 0] Title: 문자열 밀기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,17 +1,3 @@
 const solution = (A, B) => {
-    if(A === B) {
-        return 0;
-    }
-    
-    let str = A;
-    
-    for(let i = 0; i < A.length - 1; i++) {
-        str = str.at(-1) + str.slice(0, str.length - 1);
-        
-        if(str === B) {
-            return i + 1;
-        }
-    }
-    
-    return -1;
+    return (B + B).indexOf(A);
 }
