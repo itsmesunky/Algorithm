aa9c0254343d5b026c2b4c5d5c2b36d260c179c9
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-05-19 17:23:42 +0900

    [level 0] Title: 안전지대, Time: 0.36 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/README.md
+++ 프로그래머스/0/120866. 안전지대/README.md
@@ -1,10 +1,10 @@
 # [level 0] 안전지대 - 120866 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120866) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120866?gad_source=1&gad_campaignid=22499034228&gbraid=0AAAAAC_c4nC-WbRIfdFMNhNfYtjHmRIVQ&gclid=CjwKCAjwravBBhBjEiwAIr30VAHLYqvB3FNgpoVCCbop70N7SsrRLxFDi7lzP0WL6-vpEk7IbyYEGBoC1z8QAvD_BwE) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.30 ms
+메모리: 33.5 MB, 시간: 0.36 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 19일 16:07:34
+2025년 05월 19일 17:23:39
 
 ### 문제 설명
 
--- 프로그래머스/0/120866. 안전지대/안전지대.js
+++ 프로그래머스/0/120866. 안전지대/안전지대.js
@@ -1,24 +1,27 @@
 const solution = (board) => {
-    const n = board.length;
+    const len = board.length;
+    
     const dx = [-1, -1, -1, 0, 0, 1, 1, 1];
     const dy = [-1, 0, 1, -1, 1, -1, 0, 1];
-
-    const danger = Array.from({ length: n }, () => Array(n).fill(0));
-
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            if (board[i][j] === 1) {
-                danger[i][j] = 1;
-                for (let d = 0; d < 8; d++) {
-                    const ni = i + dx[d];
-                    const nj = j + dy[d];
-                    if (ni >= 0 && ni < n && nj >= 0 && nj < n) {
-                        danger[ni][nj] = 1;
+    
+    const copiedBoard = Array.from({length: len}, () => Array(len).fill(0));
+    
+    for(let i = 0; i < len; i++) {
+        for(let j = 0; j < len; j++) {
+            if(board[i][j] === 1) {
+                copiedBoard[i][j] = 1;
+                
+                for(let k = 0; k < 8; k++) {
+                    let mx = i + dx[k];
+                    let my = j + dy[k];
+                    
+                    if(mx >= 0 && mx < len && my >= 0 && my < len) {
+                        copiedBoard[mx][my] = 1;
                     }
                 }
             }
         }
     }
-
-    return danger.flat().filter(v => v === 0).length;
-};
+    
+    return copiedBoard.flat().filter(v => v === 0).length;
+}
