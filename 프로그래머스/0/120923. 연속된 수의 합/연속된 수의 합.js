f7da9c5de9124e90267e2b7c573dfee0848c22f7
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-05-19 14:22:15 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.js
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.js
@@ -1,11 +1,3 @@
 const solution = (num, total) => {
-    return Array.from({length: num})
-                .fill(parseInt(total/num))
-                .map((v, i) => {
-                        if(num % 2 === 0) {
-                            return v + (i - parseInt(num / 2 - 1));
-                        } else {
-                            return v + (i - parseInt(num / 2));
-                        }
-                    })
+    return Array.from({length: num}).fill(parseInt(total/num)).map((v, i) => num % 2 === 0 ? v + (i - parseInt(num / 2 - 1)) : v + (i - parseInt(num / 2)))
 }
