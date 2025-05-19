a5e62c5159bd52fc5b6297859e38e2cc7e553da3
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-05-19 16:07:36 +0900

    [level 0] Title: 안전지대, Time: 0.30 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.js
+++ 프로그래머스/0/120866. 안전지대/안전지대.js
@@ -1,71 +1,24 @@
 const solution = (board) => {
-    let result = 0;
-    
-    for(let i = 0; i < board.length; i++) {
-        for(let j = 0; j < board.length; j++) {
-            if(board[i][j] === 1) {
-                // 좌
-                if(j > 0) {
-                    if(board[i][j-1] !== 1) {
-                        board[i][j-1] = 2;    
-                    }
-                }
-                
-                // 우
-                if(j < board.length - 1) {
-                    if(board[i][j+1] !== 1) {
-                        board[i][j+1] = 2;
-                    }
-                }
-                
-                // 위
-                if(i > 0) {
-                    if(board[i-1][j] !== 1) {
-                        board[i-1][j] = 2;    
-                    }
+    const n = board.length;
+    const dx = [-1, -1, -1, 0, 0, 1, 1, 1];
+    const dy = [-1, 0, 1, -1, 1, -1, 0, 1];
 
-                    // 상좌
-                    if(j > 0) {
-                        if(board[i-1][j-1] !== 1) {
-                            board[i-1][j-1] = 2;    
-                        }
-                    }
-                    
-                    // 상우
-                    if(j < board.length - 1) {
-                        if(board[i-1][j+1] !== 1) {
-                            board[i-1][j+1] = 2;    
-                        }
-                    }
-                }
-                
-                // 하
-                if(i < board.length - 1) {
-                    if(board[i+1][j] !== 1) {
-                        board[i+1][j] = 2;    
-                    }
-                    
-                    // 하좌
-                    if(j > 0) {
-                        if(board[i+1][j-1] !== 1) {
-                            board[i+1][j-1] = 2;
-                        }
-                    }
-                    
-                    // 하우
-                    if(j < board.length -1) {
-                        if(board[i+1][j+1] !== 1) {
-                            board[i+1][j+1] = 2;    
-                        }
+    const danger = Array.from({ length: n }, () => Array(n).fill(0));
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (board[i][j] === 1) {
+                danger[i][j] = 1;
+                for (let d = 0; d < 8; d++) {
+                    const ni = i + dx[d];
+                    const nj = j + dy[d];
+                    if (ni >= 0 && ni < n && nj >= 0 && nj < n) {
+                        danger[ni][nj] = 1;
                     }
                 }
             }
         }
     }
-    
-    board.forEach(arr => arr.forEach(item => {
-      if(item === 0) result += 1;
-    }))
 
-    return result;
-}
+    return danger.flat().filter(v => v === 0).length;
+};
