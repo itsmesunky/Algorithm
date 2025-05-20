86e1a9080302f71548f41fee677da2e1a4977f6a
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-05-20 10:14:06 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,24 +1,12 @@
-// 최대공약수
 const getGCD = (x, y) => {
-    const max = Math.max(x, y);
-    const min = Math.min(x, y);
-
-    return max % min === 0 ? min : getGCD(min, max % min);
+    return (x % y) ? getGCD(y, x % y) : y;
 }
 
 const solution = (numer1, denom1, numer2, denom2) => {
-    const GCD = getGCD(denom1, denom2);
+    const [numer, denom] = [(numer1 * denom2) + (numer2 * denom1), denom1 * denom2];
     
-    const LCM = (denom1 * denom2) / GCD;
+    const GCD = getGCD(numer, denom);
     
-    numer1 *= (LCM / denom1);
-    numer2 *= (LCM / denom2);
-    denom1 *= (LCM / denom1);
-    denom2 *= (LCM / denom2);
-    
-    const [numer, denom] = [numer1 + numer2, denom1];
-    
-    const GCD2 = getGCD(numer, denom);
-    
-    return [numer / GCD2, denom / GCD2];
+    return [numer / GCD, denom / GCD];
 }
+
