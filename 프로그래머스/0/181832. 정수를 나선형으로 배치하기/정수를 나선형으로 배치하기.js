2b2b20da654eca6bf9ec9f09c54d154f5382f09c
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-05-21 21:19:33 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.43 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.js
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.js
@@ -1,28 +1,31 @@
-function solution(n) {
-  const answer = Array.from({ length: n }, () => Array(n).fill(0));
-
-  const dx = [0, 1, 0, -1];
-  const dy = [1, 0, -1, 0];
-
-  let x = 0, y = 0;
-  let dir = 0; 
-  let num = 1;
-
-  while (num <= n * n) {
-    answer[x][y] = num++;
-
-    const nx = x + dx[dir];
-    const ny = y + dy[dir];
-
-    if (
-      nx < 0 || ny < 0 || nx >= n || ny >= n || answer[nx][ny] !== 0
-    ) {
-      dir = (dir + 1) % 4;
+const solution = (n) => {
+    const result = Array.from({length: n}, () => Array(n).fill(0));
+    
+    // 델타 탐색(우 - 하 - 좌 - 상)
+    let dx = [0, 1, 0, -1]; // 행
+    let dy = [1, 0, -1, 0]; // 열
+    
+    let dir = 0;
+    
+    let x = 0;
+    let y = 0;
+    
+    let num = 1;
+    
+    while(num <= Math.pow(n, 2)) {
+        result[x][y] = num++;
+        
+        let mx = x + dx[dir];
+        let my = y + dy[dir];
+        
+        if(mx >= n || mx < 0 || my >= n || my < 0 || result[mx][my] !== 0) {
+            // 방향 전환
+            dir = (dir + 1) % 4;
+        }
+        
+        x += dx[dir];
+        y += dy[dir];
     }
-
-    x += dx[dir];
-    y += dy[dir];
-  }
-
-  return answer;
+    
+    return result;
 }
