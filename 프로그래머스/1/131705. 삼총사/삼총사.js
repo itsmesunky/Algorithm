f55a2384bad99f2022d48d12e56212f16bfb4954
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-05-22 17:10:27 +0900

    [level 1] Title: 삼총사, Time: 0.36 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -0,0 +1,20 @@
+const solution = (number) => {
+    const arrays = [];
+    let result = 0;
+    
+    for(let i = 0;  i <= number.length - 3; i++) {
+        for(let j = i + 1; j <= number.length - 2; j++) {
+            for(let k = j + 1; k <= number.length - 1; k++) {
+                arrays.push([number[i], number[j], number[k]]);
+            }
+        }
+    }
+    
+    arrays.forEach(item => {
+        const sum = item.reduce((acc, cur) => acc += cur, 0);
+        
+        if(sum === 0) result++;
+    });
+    
+    return result;
+}
