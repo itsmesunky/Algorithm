34950a17cd6d6f8b3cab97ac04d16bd3217172ef
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-05-25 23:42:46 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,28 +1,11 @@
-const getMeasure = (num) => {
-    const measure = [];
-    
-    let i = 1;
-    
-    while(i <= Math.sqrt(num)) {
-        if(num % i === 0) {
-            measure.push(i);
-            
-            if(i !== num / i)  measure.push(num / i);
-        }
-        i++;
-    }
-    
-    return measure.length;
-}
-
 const solution = (left, right) => {
     let result = 0;
     
     for(let i = left; i <= right; i++) {
-        if(getMeasure(i) % 2 === 0) {
-            result += i;
-        } else {
+        if(Number.isInteger(Math.sqrt(i))) {
             result -= i;
+        } else {
+            result += i;
         }
     }
     
