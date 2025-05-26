ef3ca86411110b0d8c1ceb207164c48e5b61fdbe
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-05-26 15:34:13 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -7,11 +7,9 @@ const solution = (s) => {
             return acc += " ";
         }
         
-        if(i % 2 === 0) {
-            i++;
+        if(i++ % 2 === 0) {
             return acc += cur.toUpperCase();
         } else {
-            i++;
             return acc += cur.toLowerCase();
         }
         
