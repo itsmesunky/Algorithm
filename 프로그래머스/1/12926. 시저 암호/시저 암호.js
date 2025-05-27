e6c0ea3855538e8294726894df5f15e8d5c8a812
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-05-27 10:40:16 +0900

    [level 1] Title: 시저 암호, Time: 22.00 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -1,25 +1,8 @@
 const solution = (s, n) => {
-    let mod = "lower";
-    const alphabet = Array.from({length: 26}, (_, i) => String.fromCharCode(i + 65));
-    
     return [...s].map(char => {
         if(char === " ") return " ";
         
-        if(/[A-Z]/.test(char)) {
-            mod = "upper";
-        } else {
-            mod = "lower";
-        }
-        
-        let mIdx = alphabet.indexOf(char.toUpperCase()) + n;
-        
-        if(mIdx > 25) mIdx -= 26;
-        
-        if(mod === "lower") {
-            return alphabet[mIdx].toLowerCase();
-        } else {
-            return alphabet[mIdx];
-        }
-        
+        const base = char === char.toUpperCase() ? 65 : 97;
+        return String.fromCharCode((char.charCodeAt(0) - base + n) % 26 + base);
     }).join('');
 }
