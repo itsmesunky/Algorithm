0e43e6546d6e27fd9e41a0b6a56156de41ad58d1
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-05-27 16:01:41 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 16.93 ms, Memory: 36 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,13 +1,3 @@
 const solution = (strings, n) => {
-    return strings.sort((a, b) => {
-        const [charCodeA, charCodeB] = [a[n].charCodeAt(0), b[n].charCodeAt(0)]
-        
-        if(charCodeA - charCodeB < 0) {
-            return -1;
-        } else if(charCodeA - charCodeB > 0) {
-            return 1;
-        } else {
-            return a.localeCompare(b);
-        }
-    });
+    return strings.sort((a, b) => a[n] === b[n] ? a.localeCompare(b) : a[n].localeCompare(b[n]));
 }
