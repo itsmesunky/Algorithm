a04c55e50bb0c27afdb316ae39b5930c902b965b
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-05-28 15:27:23 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,15 +1,6 @@
 const solution = (n, arr1, arr2) => {
-    const map = Array.from({length: n}, () => Array(n).fill(0));
-    
-    arr1.forEach((num, i) => num.toString(2)
-                                .padStart(n, 0)
-                                .split('')
-                                .forEach((bin, j) => map[i][j] = bin));
-    
-    arr2.forEach((num, i) => num.toString(2)
-                                .padStart(n, 0)
-                                .split('')
-                                .forEach((bin, j) => map[i][j] === "1" ? map[i][j] = "1" : map[i][j] = bin));
-    
-    return map.map(arr => arr.reduce((acc, cur) => cur === "0" ? acc += " " : acc += "#", ""));
+    return arr1.map((num, i) => {
+        const binary = (num | arr2[i]).toString(2).padStart(n, 0);
+        return [...binary].map((char) => char === '0' ? " " : "#").join('');
+    })
 }
