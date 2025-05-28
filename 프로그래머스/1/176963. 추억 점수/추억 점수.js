f5943b55cab0061378746659e9c14cb07cb327dc
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-05-28 16:14:44 +0900

    [level 1] Title: 추억 점수, Time: 1.87 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -1,7 +1,4 @@
 const solution = (name, yearning, photo) => {
-    const obj = {};
-    
-    name.forEach((v, i) => obj[v] = yearning[i]);
-    
+    const obj = Object.fromEntries(name.map((v, i) => [v, yearning[i]]));
     return photo.map(arr => arr.reduce((acc, cur) => acc += obj[cur] || 0, 0));
 }
