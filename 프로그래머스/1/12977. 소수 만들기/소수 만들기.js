8b1fe9e17bcdb221e65ab8c5a2dba91c37b79a18
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-05-29 16:06:42 +0900

    [level 1] Title: 소수 만들기, Time: 6.68 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -1,22 +1,21 @@
-const solution = (nums) => {
-    let result = 0;
-    
-    const isPrime = Array(3001).fill(true);
-    isPrime[0] = isPrime[1] = false;
-    
-    for(let i = 2; i <= isPrime.length; i++) {
-        if(isPrime[i]) {
-            for(let j = i * 2; j <= isPrime.length; j += i) {
-                isPrime[j] = false;
-            }
+const isPrime = (num) => {
+    for(let i = 2; i <= Math.sqrt(num); i++) {
+        if(!(num % i)) {
+            return false;
         }
     }
+    
+    return true;
+}
+
+const solution = (nums) => {
+    let result = 0;
     
     for(let i = 0; i < nums.length - 2; i++) {
         for(let j = i + 1; j < nums.length - 1; j++) {
             for(let k = j + 1; k < nums.length; k++) {
                 const sum = nums[i] + nums[j] + nums[k];
-                if(isPrime[sum]) result++;
+                if(isPrime(sum)) result++;
             }
         }
     }
