849f16c10d1f3a7bb84adb488ea0eaa0f530edea
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-06-03 18:08:10 +0900

    [level 1] Title: 실패율, Time: 5634.46 ms, Memory: 67.3 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,11 +1,12 @@
 const solution = (N, stages) => {
-    const array = [];
+    const failureRates = [];
     
     for(let i = 1; i <= N; i++) {
-        let notClearPlayer = stages.filter(v => v === i).length;
-        let reachPlayer = stages.filter(v => v >= i).length;
-        array.push([i, (notClearPlayer / reachPlayer).toFixed(20)]);
+        const notClearPlayer = stages.filter(v => v === i).length;
+        const reachPlayer = stages.filter(v => v >= i).length;
+        failureRates.push([i, (notClearPlayer / reachPlayer)]);
     }
     
-    return array.sort((a, b) => a[1] === b[1] ? a[0] - b[0] : b[1] - a[1]).map(([stage, failureRate]) => stage)
+    return failureRates.sort((a, b) => b[1] - a[1] || a[0] - b[0])
+                        .map(([stage, failureRate]) => stage)
 }
