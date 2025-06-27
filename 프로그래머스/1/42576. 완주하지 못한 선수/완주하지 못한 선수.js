3861639551cd6bbb82ac3f2be898851f7f21ebdb
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-06-27 16:12:01 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 66.42 ms, Memory: 66 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -2,18 +2,22 @@ const solution = (participant, completion) => {
     const participantMap = new Map();
     
     for(const name of participant) {
-        participantMap.set(name, (participantMap.get(name) ?? 0) + 1);
+        const value = participantMap.get(name);
+        
+        participantMap.set(name, (value ?? 0) + 1);
     }
     
     for(const name of completion) {
-        if(participantMap.get(name)) {
-            participantMap.set(name, participantMap.get(name) - 1);
+        const value = participantMap.get(name);
+        
+        if(value) {
+            participantMap.set(name, value - 1);
         } else {
             return name;
         }
     }
     
-    for(const [name, count] of [...participantMap]) {
+    for(const [name, count] of participantMap) {
         if(count !== 0) {
             return name;      
         }
