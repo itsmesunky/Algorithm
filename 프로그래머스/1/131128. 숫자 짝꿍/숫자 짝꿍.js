9e5b00cc0bd7ebfef3900243e06dea7a3d41a710
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-06-27 22:11:54 +0900

    [level 1] Title: 숫자 짝꿍, Time: 44.48 ms, Memory: 60.3 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -3,23 +3,26 @@ const solution = (X, Y) => {
     const yArr = Array(10).fill(0);
     
     for(let i = 0; i < X.length; i++) {
-        xArr[X[i]]++;
+        xArr[Number(X[i])]++;
     }
     
     for(let i = 0; i < Y.length; i++) {
-        yArr[Y[i]]++;
+        yArr[Number(Y[i])]++;
     }
     
     let answer = "";
     
     for(let i = 9; i >= 0; i--) {
-        while(xArr[i] > 0 && yArr[i] > 0) {
-            answer += i;
-            xArr[i]--;
-            yArr[i]--;
-        }
+        const commonCount = Math.min(xArr[i], yArr[i]);
+        
+        answer += i.toString().repeat(commonCount);
     }
     
-    return answer ? (+answer > 0 ? answer : "0") : "-1";
-    
+    if(!answer) {
+        return "-1";
+    } else if(answer[0] === "0") {
+        return "0";
+    } else {
+        return answer;
+    }
 }
