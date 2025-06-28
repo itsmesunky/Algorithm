83bfe587fb7da8cab2dbb530f45b770b1bfdbd74
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-06-28 19:11:59 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 31.22 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -0,0 +1,33 @@
+const solution = (board, moves) => {
+    let bucket = []; // 뽑은 인형을 담을 배열
+    let count = 0; // 터뜨려진 인형의 갯수
+    let doll = 0; // 뽑은 인형
+    
+    // 이동 배열 순회
+    for(let i = 0; i < moves.length; i++) {
+        // board행 순회
+        for(let j = 0; j < board.length; j++) {
+            if(board[j][moves[i] - 1] === 0) {
+                continue;
+            } else {
+                doll = board[j][moves[i] - 1];
+                board[j][moves[i] - 1] = 0;
+                break;
+            }
+        }
+        
+        if(doll === 0) continue;
+        
+        // 바구니 제일 위에 있는 인형이 뽑은 인형과 같다면
+        if(bucket.at(-1) === doll) {
+            bucket.pop();
+            count += 2;
+        } else {
+            bucket.push(doll);
+        }
+        
+        doll = 0;
+    }
+    
+    return count;
+}
