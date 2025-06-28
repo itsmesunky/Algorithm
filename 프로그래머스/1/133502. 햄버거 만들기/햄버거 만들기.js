33cebf700a56c648196d486bfbb3ef85ca6e1130
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-06-28 18:31:51 +0900

    [level 1] Title: 햄버거 만들기, Time: 51.31 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -1,17 +1,16 @@
-function solution(ingredient) {
-    let stk = [];
-    let count = 0;
-    for (let i = 0; i < ingredient.length; i++) {
-        stk.push(ingredient[i]);
-        if (
-            stk[stk.length-1] === 1 &&
-            stk[stk.length-2] === 3 &&
-            stk[stk.length-3] === 2 &&
-            stk[stk.length-4] === 1
-        ) {
-            count++;
-            stk.splice(-4);
+const solution = (ingredient) => {
+    let answer = 0;
+    const stack = [];
+    
+    for(let i = 0; i < ingredient.length; i++) {
+        stack.push(ingredient[i]);
+        
+        if(stack[stack.length - 4] === 1 && stack[stack.length - 3] === 2
+           && stack[stack.length - 2] === 3 && stack[stack.length - 1] === 1) {
+            stack.splice(stack.length - 4);
+            answer++;
         }
     }
-    return count;
+    
+    return answer;
 }
