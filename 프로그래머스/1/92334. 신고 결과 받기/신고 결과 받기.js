ad920bfff580dd6aa4f033b6ecfd673053b32eeb
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-07-06 20:44:52 +0900

    [level 1] Title: 신고 결과 받기, Time: 1134.64 ms, Memory: 85.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -0,0 +1,30 @@
+const solution = (id_list, report, k) => {
+    const users = Object.fromEntries(id_list.map(name => [name, {count: 0, reports: new Set()}]));
+    
+    const blacklist = new Set();
+    
+    const answer = [];
+    
+    // 유저별 신고 당한 횟수 및 신고한 유저 저장
+    for(const names of report) {
+        const [reporter, reported] = names.split(" ");
+        
+        if(!users[reporter].reports.has(reported)) {
+            users[reporter].reports.add(reported);
+            users[reported].count++;    
+        }
+        
+        // K번 이상 신고 당한 유저 저장
+        if(users[reported].count >= k)  {
+            blacklist.add(reported);
+        }
+    }
+    
+    for(const name in users) {
+        const count = [...users[name].reports].filter(v => [...blacklist].indexOf(v) > -1).length;
+        
+        answer.push(count);
+    }
+    
+    return answer;
+}
