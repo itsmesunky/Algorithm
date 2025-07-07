fdb6a0b40be0e892fc29c8fd11cec68e52c89492
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-07-07 21:15:03 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.30 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
@@ -1,41 +1,4 @@
-const solution = (n, w, num) => {
-    const arr = Array.from({length: w + 1}, () => []);
-    
-    // 상자 쌓은 횟수
-    let count = 1;
-    
-    // 방향
-    let dir = "right";
-    
-    // 키
-    let key;
-    
-    // 상자 쌓을 위치
-    let idx = 1;
-    
-    while(count <= n) {
-        if(count === num) {
-            key = idx;
-        }
-        
-        arr[idx].push(count);
-        
-        if(dir === 'right') {
-            idx++;
-        } else {
-            idx--;
-        }
-        
-        if(idx < 1) { // 상자 쌓을 위치가 맨 왼쪽인 경우
-            idx++;
-            dir = "right";
-        } else if(idx > w) { // 상자 쌓을 위치가 맨 오른쪽을 벗어난 경우
-            idx--;
-            dir = "left";
-        }
-        
-        count++;
-    }
-    
-    return arr[key].filter(v => v >= num).length;
+function solution(n, w, num) {
+    var answer = 0;
+    return answer;
 }
