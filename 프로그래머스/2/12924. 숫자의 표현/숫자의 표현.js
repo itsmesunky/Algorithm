58e673e54bd35d3c99a57c6083156913691f6208
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-07-13 17:36:12 +0900

    [level 2] Title: 숫자의 표현, Time: 0.18 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,20 +1,11 @@
 const solution = (n) => {
-    let answer = 1;
-    let num = Math.ceil(n / 2);
-    let sum = 0;
+    let count = 0;
     
-    for(let i = 1; i < num; i++) {
-        sum = 0;
-        for(let j = i; j <= num; j++) {
-            sum += j;
-            
-            if(sum > n) break;
-            if(sum === n) {
-                answer++;
-                break;
-            }
-        }
+    for(let i = 1; i * (i - 1) / 2 < n; i++) {
+        const num = (n - i * (i - 1) / 2) / i;
+        
+        if(!(num % 1)) count++;
     }
     
-    return answer;
+    return count;
 }
