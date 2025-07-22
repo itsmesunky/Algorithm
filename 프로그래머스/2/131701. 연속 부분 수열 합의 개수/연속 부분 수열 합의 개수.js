6efef7cf5f8b4ec6578c67d5916dadc250ca51dc
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-07-22 11:32:04 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 894.14 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,14 +1,17 @@
 const solution = (elements) => {
     const len = elements.length;
-    const circular = elements.concat(elements); // 원형 처리를 위해 두 번 이어붙임
+    const arr = elements.concat(elements);
     const set = new Set();
-
-    for (let i = 1; i <= len; i++) { // 부분 수열 길이
-        for (let start = 0; start < len; start++) { // 시작 인덱스
-            const sum = circular.slice(start, start + i).reduce((acc, cur) => acc + cur, 0);
+    
+    for(let i = 1; i <= len; i++) {
+        let pos = 0;
+        
+        while(pos < len) {
+            let sum = arr.slice(pos, pos + i).reduce((acc, cur) => acc += cur, 0);
             set.add(sum);
+            pos++;
         }
     }
-
+    
     return set.size;
-};
+}
