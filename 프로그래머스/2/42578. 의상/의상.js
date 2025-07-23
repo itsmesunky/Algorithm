da76bd56393bb0c55f4a434a0079380043ab5ac6
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-07-23 15:09:00 +0900

    [level 2] Title: 의상, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -6,8 +6,5 @@ const solution = (clothes) => {
         obj[div] = (obj[div] ?? 0) + 1;
     }
     
-    // 의상 종류 갯수
-    const divCnt = Object.keys(obj).length;
-    
     return Object.values(obj).reduce((acc, cur) => acc * (cur + 1), 1) - 1;
 }
