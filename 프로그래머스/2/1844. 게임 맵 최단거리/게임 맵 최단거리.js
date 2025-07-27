07ab3979bb6ed16e8a4d077be37bd0a5d927c843
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-07-27 16:56:26 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.14 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,29 +1,50 @@
+/**
+* 최단 거리 탐색을 위해 BFS(너비 우선 탐색) 알고리즘 적용
+*/
 const solution = (maps) => {
-    const n = maps.length; // 행 (세로)
-    const m = maps[0].length; // 열 (가로)
-
-    const dx = [1, -1, 0, 0]; // x축 이동 (오른쪽, 왼쪽)
-    const dy = [0, 0, 1, -1]; // y축 이동 (아래, 위)
-
-    const queue = [[0, 0, 1]];
-
-    while (queue.length > 0) {
-        const [x, y, dist] = queue.shift(); // 큐에서 현재 위치와 현재까지의 거리 꺼내기
-
-        if (x === n - 1 && y === m - 1) {
-            return dist;
-        }
-
-        for (let i = 0; i < 4; i++) {
-            const nx = x + dx[i]; // 다음 x 좌표
-            const ny = y + dy[i]; // 다음 y 좌표
-
-            if (nx >= 0 && nx < n && ny >= 0 && ny < m && maps[nx][ny] === 1) {
-                maps[nx][ny] = dist + 1;
-                queue.push([nx, ny, dist + 1]);
+    const n = maps.length; // 행의 크기
+    const m = maps[0].length; // 열의 크기
+    
+    // 델타 탐색을 위한 동서남북 정의
+    const dx = [1, -1, 0, 0];
+    const dy = [0, 0, 1, -1];
+    
+    const queue = []; // 노드를 저장할 Queue
+    
+    const bfs = (map) => {
+        // 행, 열, 시작점으로부터 거리
+        queue.push([0, 0, 1]);
+        
+        // 시작점 방문 처리
+        map[0][0] = 0;
+        
+        while(queue.length) {
+            const [row, column, distance] = queue.shift();
+            
+            // 동서남북 탐색
+            for(let i = 0; i < 4; i++) {
+                const nx = column + dx[i];
+                const ny = row + dy[i];
+                
+                // maps 영역을 벗어났을 때 다음 방향 탐색
+                if(nx < 0 || ny < 0 || nx >= m || ny >= n) {
+                    continue;
+                }
+                
+                // 해당 방향 탐색 가능하면 queue에 추가
+                if(map[ny][nx] !== 0) {
+                    queue.push([ny, nx, distance + 1]);
+                    map[ny][nx] = 0;
+                }
+                
+                if(nx === m - 1 && ny === n - 1) {
+                    return distance + 1;
+                }
             }
         }
+        
+        return -1;
     }
-
-    return -1;
+    
+    return bfs(maps);
 }
