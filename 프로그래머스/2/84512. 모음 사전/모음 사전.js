b4343000502deba999aa0e8ba15e6ad0f4542704
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-07-30 14:10:09 +0900

    [level 2] Title: 모음 사전, Time: 0.77 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,32 +1,20 @@
 const solution = (word) => {
-    const chars = [];
-    const gather = ['A', 'E', 'I', 'O', 'U'];
     let answer = 0;
-    let flag = false;
+    const gather = ['A', 'E', 'I', 'O', 'U'];
     
-    const dfs = (idx) => {
+    const dfs = (currentWord) => {
+        if(currentWord === word) return true;
+        if(currentWord.length === 5) return false;
+        
         for(let i = 0; i < gather.length; i++) {
-            chars[idx] = gather[i];
             answer++;
-            
-            if(chars.join('') === word) {
-                flag = true;
-                return;
-            }
-            
-            if(idx === gather.length - 1) {
-                continue;
-            }
-            
-            dfs(idx + 1);
-
-            if(flag) return;
+            if(dfs(currentWord + gather[i])) return true;
         }
         
-        chars[idx] = '';
-        
+        return false;
     }
     
-    dfs(0);
+    dfs('');
+    
     return answer;
-}
+};
