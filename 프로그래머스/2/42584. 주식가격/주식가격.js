86c00d602a3bb284cbe71939537c9ead0faf1e5c
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-08-04 12:07:19 +0900

    [level 2] Title: 주식가격, Time: 30.29 ms, Memory: 44.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -0,0 +1,19 @@
+const solution = (prices) => {
+    const answer = [];
+    
+    for(let i = 0; i< prices.length; i++) {
+        let sec = 0;
+        
+        for(let j = i + 1; j < prices.length; j++) {
+            sec++;
+            
+            if(prices[j] < prices[i]) {
+                break;
+            }
+        }
+        
+        answer.push(sec);
+    }
+    
+    return answer;
+}
