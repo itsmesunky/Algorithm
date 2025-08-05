93bb516911213c924ccf79f66ea4867695a83568
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-08-05 20:41:17 +0900

    [level 2] Title: 스킬트리, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.js
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.js
@@ -1,7 +1,7 @@
 const solution = (skill, skill_trees) => {
     let answer = 0;
     
-    const regExp = eval(`/[^${skill}]/g`);
+    const regExp = new RegExp(`[^${skill}]`, 'g');
     
     for(const s of skill_trees) {
         const filtered = s.replaceAll(regExp, '');
