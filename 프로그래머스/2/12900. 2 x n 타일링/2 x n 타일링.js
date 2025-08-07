0cbc00fbc34881996014e7f618aba27ab8347987
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-08-07 12:14:05 +0900

    [level 2] Title: 2 x n 타일링, Time: 1.54 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
@@ -1,17 +1,13 @@
 const solution = (n) => {
-    if(n === 1 || n === 2) {
-        return n;
-    }
+    if(n <= 2) return n;
     
     let a = 1, b = 2;
-    
-    let answer;
-        
+
     for(let i = 3; i <= n; i++) {
-        answer = (a + b) % 1000000007;
+        const next = (a + b) % 1000000007;
         a = b;
-        b = answer;
+        b = next;
     }
-    
-    return answer;
+
+    return b;
 }
