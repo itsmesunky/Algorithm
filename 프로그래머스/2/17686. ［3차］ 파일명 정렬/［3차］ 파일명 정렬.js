f8d533e22cb14b476e5b592903b20f79b72db329
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-08-08 14:52:27 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 49.48 ms, Memory: 39.7 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -1,35 +1,21 @@
-/**
-* separateString: 문자열이 주어지면 head, number, tail로 분리해주는 헬퍼 함수
-* param(str): 분리를 원하는 문자열
-*/
-const separateString = (str) => {
-    let head = '';
-    let number = '';
-    let tail = '';
+const separateString = (originalText) => {
+    /**
+    * 첫 번째 그룹(Head): 영문 대소문자, 공백, 마침표, 빼기 부호
+    * 두 번째 그룹(Number): 숫자 1자리부터 5자리까지
+    * 세 번째 그룹(Tail): 모든 문자
+    */
+    const regExp = /^([a-zA-Z-. ]+)(\d{1,5})(.*)/
+    const [_, head, number, tail] = originalText.match(regExp);
     
-    for(const char of str) {
-        const isNaN = Number.isNaN(+char) || char === " ";
-        
-        if(isNaN && !number.length) {
-            head += char;
-        } else if(!isNaN && number.length < 5 && !tail.length) {
-            number += char;
-        } else {
-            tail += char;
-        }
-    }
-    
-    return [head, number, tail];
+    return { originalText, head, number, tail }
 }
 
 const solution = (files) => {
     return files.map(file => separateString(file))
-                .sort(([headA, numberA], [headB, numberB]) => {
-                    if(headA.toLowerCase() === headB.toLowerCase()) { // 두 문자열이 같을 때
-                        return parseInt(numberA) - parseInt(numberB);
-                    } else { // 두 문자열이 다를 때 
-                        return headA.localeCompare(headB, 'en-US', {sensitivity: 'base'});
-                    }
+                .sort((a, b) => {
+                    const [headA, headB] = [a.head.toLowerCase(), b.head.toLowerCase()];
+        
+                    return headA === headB ? +a.number - +b.number : headA.localeCompare(headB);
                 })
-                .map(v => v.join(''));
-}
+                .map(file => file.originalText);
+};
