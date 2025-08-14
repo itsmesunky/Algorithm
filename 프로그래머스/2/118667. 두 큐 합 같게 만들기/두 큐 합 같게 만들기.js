d2b804b96caac084e2741944bd2ec3ddec774b19
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-08-14 16:35:40 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 60.85 ms, Memory: 67.7 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -3,7 +3,7 @@ const solution = (queue1, queue2) => {
     const joined = [...queue1, ...queue2];
     
     // 모든 원소의 합
-    const allSum = joined.reduce((acc, cur) => cur += acc, 0);
+    const allSum = joined.reduce((acc, cur) => acc + cur, 0);
     
     // 모든 원소의 합이 홀수인 경우 두 개의 큐로 나눌 수 없으므로 -1을 반환
     if(allSum % 2 !== 0) return -1;
@@ -11,7 +11,7 @@ const solution = (queue1, queue2) => {
     const goal = allSum / 2;
     
     // 초기 합: 큐1 원소의 합
-    let sum = queue1.reduce((acc, cur) => cur += acc, 0);
+    let sum = queue1.reduce((acc, cur) => acc + cur, 0);
     
     let count = 0;
     let startIdx = 0;
