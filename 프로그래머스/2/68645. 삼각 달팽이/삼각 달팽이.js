b04e6401c8b9ea92128539c55d082d09b0ad8eb8
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-08-16 17:32:55 +0900

    [level 2] Title: 삼각 달팽이, Time: 85.34 ms, Memory: 98.4 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
@@ -1,28 +1,25 @@
-function solution(n) {
-  const result = [];
-  for (let i = 0; i < n; i++) {
-    result.push(Array(i + 1).fill(0));
-  }
-
-  let x = -1;
-  let y = 0;
-  let num = 0;
-
-  for (let i = 0; i < n; i++) {
-    for (let j = i; j < n; j++) {
-      num++;
-      if (i % 3 === 0) { // 아래로 이동
-        x++;
-      } else if (i % 3 === 1) { // 오른쪽으로 이동
-        y++;
-      } else { // 위로 이동 (대각선)
-        x--;
-        y--;
-      }
-      result[x][y] = num;
+const solution = (n) => {
+    const result = Array.from({length: n}, (_, i) => Array(i + 1).fill(0));
+    
+    let row = -1, col = 0, num = 0;
+    
+    for(let i = 0; i < n; i++) {
+        for(let j = i; j < n; j++) {
+            num++;
+            
+            if(i % 3 === 0) { // 아래로 이동
+                row++;
+            } else if(i % 3 === 1) { // 오른쪽으로 이동
+                col++;
+            } else { // 위로 이동
+                row--;
+                col--;
+            }
+            
+            result[row][col] = num;
+        }
     }
-  }
-
-  // 2차원 배열을 1차원 배열로 변환
-  return result.flatMap((v) => v);
+    
+    return result.flat();
+    
 }
