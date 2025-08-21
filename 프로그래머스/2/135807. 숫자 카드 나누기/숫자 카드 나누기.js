b6974bd8adddda4f75a2e72ae0180574e437d64f
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-08-21 17:27:13 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 296.89 ms, Memory: 37.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.js
@@ -0,0 +1,40 @@
+// 최대공약수 찾기
+const GCD = (a, b) => a % b ? GCD(b, a % b) : b;
+
+const solution = (arrayA, arrayB) => {
+    let gcdA; // 철수가 가진 카드의 최대 공약수를 저장
+    let gcdB; // 영희가 가진 카드의 최대 공약수를 저장
+    
+    // 정답 후보가 될 수 있는 수
+    const answers = [];
+    
+    // 카드가 각각 한 개인 경우
+    if(arrayA.length === 1) {
+        gcdA = arrayA[0];
+        gcdB = arrayB[0];
+    } else {
+        gcdA = GCD(arrayA[0], arrayA[1]);
+        gcdB = GCD(arrayB[0], arrayB[1]);
+    }
+    
+    for(let i = 2; i < arrayA.length; i++) {
+        gcdA = GCD(gcdA, arrayA[i]);
+        gcdB = GCD(gcdB, arrayB[i]);
+    }
+    
+    if(gcdA > 1) { // 철수가 가진 카드들에 적힌 모든 숫자를 나눌 수 있는 수
+        // 영희가 가진 카드들은 모두 나눠지지 않아야 함
+        if(!(arrayB.some(v => v % gcdA === 0))) {
+            answers.push(gcdA);
+        }
+    }
+    
+    if(gcdB > 1) { // 영희가 가진 카드들에 적힌 모든 숫자를 나눌 수 있는 수
+        // 철수가 가진 카드들은 모두 나눠지지 않아야 함
+        if(!(arrayA.some(v => v % gcdB === 0))) {
+            answers.push(gcdB);
+        }
+    }
+    
+    return answers.length ? Math.max(...answers) : 0;
+}
