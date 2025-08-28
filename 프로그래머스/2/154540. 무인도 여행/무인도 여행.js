3fcef4bf4478ef5b5e3ceb83daec3a91d8d4e687
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-08-28 16:36:18 +0900

    [level 2] Title: 무인도 여행, Time: 13.80 ms, Memory: 38 MB -BaekjoonHub

---
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.js
@@ -0,0 +1,50 @@
+// BFS 알고리즘 활용
+const solution = (maps) => {
+    const answer = [];
+    
+    const R = maps.length; // 행 크기
+    const C = maps[0].length; // 열 크기
+    
+    // 4방향 정의
+    const dy = [-1, 1, 0, 0];
+    const dx = [0, 0, -1, 1];
+    
+    const visited = Array.from({length: R}, () => Array(C).fill(false));
+    
+    const bfs = (row, col) => {
+        const queue = [];
+        queue.push([row, col]);
+        
+        // 방문 처리
+        visited[row][col] = true;
+        
+        let sum = +maps[row][col];
+        
+        while(queue.length) {
+            const [startRow, startCol] = queue.shift();
+            
+            for(let i = 0; i < 4; i++) {
+                const ny = startRow + dy[i];
+                const nx = startCol + dx[i];
+                
+                if(!(ny < 0 || ny >= R || nx < 0 || nx >= C) && !visited[ny][nx] && maps[ny][nx] !== 'X') {
+                    visited[ny][nx] = true;
+                    sum += parseInt(maps[ny][nx])
+                    queue.push([ny, nx]);
+                }
+            }
+        }
+        
+        if(sum) answer.push(sum);
+    }
+    
+    for(let row = 0; row < R; row++) {
+        for(let col = 0; col < C; col++) {
+            if(!visited[row][col] && maps[row][col] !== 'X') {
+                bfs(row, col);
+            }
+        }
+    }
+    
+    return answer.length ? answer.sort((a, b) => a - b) : [-1];
+}
