43e8921752d7c9fb929891f0879b0fb7cb9d0cd5
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-08-28 16:41:28 +0900

    [level 2] Title: 무인도 여행, Time: 33.07 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.js
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.js
@@ -18,10 +18,11 @@ const solution = (maps) => {
         // 방문 처리
         visited[row][col] = true;
         
-        let sum = +maps[row][col];
+        let sum = 0;
         
         while(queue.length) {
             const [startRow, startCol] = queue.shift();
+            sum += parseInt(maps[startRow][startCol]);
             
             for(let i = 0; i < 4; i++) {
                 const ny = startRow + dy[i];
@@ -29,13 +30,12 @@ const solution = (maps) => {
                 
                 if(!(ny < 0 || ny >= R || nx < 0 || nx >= C) && !visited[ny][nx] && maps[ny][nx] !== 'X') {
                     visited[ny][nx] = true;
-                    sum += parseInt(maps[ny][nx])
                     queue.push([ny, nx]);
                 }
             }
         }
         
-        if(sum) answer.push(sum);
+        answer.push(sum);
     }
     
     for(let row = 0; row < R; row++) {
