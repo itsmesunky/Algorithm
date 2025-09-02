555899ef38c484a8e6549a37463222f74b12d3ee
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-09-02 17:22:28 +0900

    [level 2] Title: 테이블 해시 함수, Time: 45.64 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.js
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.js
@@ -1,31 +1,6 @@
 const solution = (data, col, row_begin, row_end) => {
-
-    const sorted = [...data].sort((a, b) => {
-        const [col1, col2] = [a[col - 1], b[col - 1]];
-
-        if(col1 === col2) {
-            return b[0] - a[0];
-        }
-        return col1 - col2;
-    });
-
-    for(let i = row_begin - 1; i < row_end; i++) {
-        for(let j = 0; j < data[i].length; j++) {
-            const value = sorted[i][j];
-            sorted[i][j] = value % (i + 1);
-        }
-        
-        const sum = sorted[i].reduce((acc, cur) => acc + cur, 0);
-        
-        sorted[i] = [];
-        sorted[i].push(sum);
-    }
-
-    const sliced = sorted.slice(row_begin - 1, row_end).flat();
-
-    while(sliced.length > 1) {
-        sliced.splice(0, 2, sliced[0] ^ sliced[1]);
-    }
-
-    return sliced[0];
+    return [...data].sort((a, b) => a[col-1] - b[col-1] || b[0] - a[0])
+                    .map((v, row) => v.reduce((acc, cur) => acc += (cur % (row + 1)), 0))
+                    .slice(row_begin - 1, row_end)
+                    .reduce((acc, cur) => acc ^= cur, 0);
 }
