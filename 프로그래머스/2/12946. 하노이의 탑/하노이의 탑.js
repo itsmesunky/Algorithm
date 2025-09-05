b1a26ce0ef316feba7e70db057d6d435bedbeaea
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-09-05 10:18:37 +0900

    [level 2] Title: 하노이의 탑, Time: 6.34 ms, Memory: 48.5 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.js
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.js
@@ -1,30 +1,20 @@
-/**
- * @param {number} n 옮겨야 할 원판의 개수
- * @param {number} from 출발지 기둥
- * @param {number} to 목적지 기둥
- * @param {number} via 보조 기둥
- * @param {number[][]} moves 이동 경로를 저장할 배열
- */
-function hanoi(n, from, to, via, moves) {
-    // 재귀의 종료 조건: 원판이 1개일 때
-    if (n === 1) {
-        moves.push([from, to]);
-        return;
+const solution = (n) => {
+    const answer = [];
+    
+    const hanoi = (num, from, to, via) => {
+        if(num === 0) return;
+        
+        // 맨 아래 원반을 제외한 모든 원반을 보조 기둥으로 이동
+        hanoi(num - 1, from, via, to);
+        
+        // 맨 아래 원반을 도착 기둥으로 이동
+        answer.push([from, to]);
+        
+        // 보조 기둥에 있던 원반들을 도착 기둥으로 이동
+        hanoi(num - 1, via, to, from);
     }
-
-    // 1. n-1개의 원판을 출발지(from)에서 보조(via)로 이동
-    hanoi(n - 1, from, via, to, moves);
-
-    // 2. 가장 큰 n번째 원판을 출발지(from)에서 목적지(to)로 이동
-    moves.push([from, to]);
-
-    // 3. n-1개의 원판을 보조(via)에서 목적지(to)로 이동
-    hanoi(n - 1, via, to, from, moves);
-}
-
-function solution(n) {
-    const moves = [];
-    // 1, 2, 3은 문제에서 제시된 기둥 번호
-    hanoi(n, 1, 3, 2, moves);
-    return moves;
+    
+    hanoi(n, 1, 3, 2);
+    
+    return answer;
 }
