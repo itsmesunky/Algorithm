016426b98fa351e685e5270c4db71131a55989f9
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-09-10 17:35:36 +0900

    [level 2] Title: 점 찍기, Time: 10.04 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.js
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.js
@@ -1,10 +1,10 @@
-function solution(k, d) {
-  let answer = 0;
-
-  for (let x = 0; x <= d; x += k) {
-    const yMax = Math.floor(Math.sqrt(d * d - x * x));
-    answer += Math.floor(yMax / k) + 1;
-  }
-
-  return answer;
+const solution = (k, d) => {
+    let answer = 0;
+    
+    for(let x = 0; x <= d; x += k) {
+        const yMax = Math.sqrt(d ** 2 - x ** 2);
+        answer += Math.floor(yMax / k) + 1;
+    }
+    
+    return answer;
 }
