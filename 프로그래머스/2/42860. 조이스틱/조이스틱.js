898fbd387674a77685308488f7bfe8d45a3e92bc
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-09-19 13:55:30 +0900

    [level 2] Title: 조이스틱, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.js
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.js
@@ -1,26 +1,31 @@
 const solution = (name) => {
     const len = name.length;
     
-    // ▲, ▼ 최소 조작 횟수
+    // ▲, ▼ 최소 조작 횟수 계산
     const upAndDown = [...name].reduce((acc, cur) => {
         const charCode = cur.charCodeAt();
         return acc + Math.min(charCode - 65, 91 - charCode);
     }, 0);
     
-    // ◀, ▶ 최소 조작 횟수
-    let leftAndRight = len - 1; // default: 직진
+    // ◀, ▶ 최소 조작 횟수 계산
+    let leftAndRight = len - 1; // 기본값: 오른쪽으로만 이동
+    
     for(let cursor = 0; cursor < len; cursor++) {
+        // 다음으로 변경해야 할 문자의 위치
         let next = cursor + 1;
+        
+        // 'A' 스킵
         while(next < len && name[next] === 'A') {
             next++;
         }
         
-        // 문자열 끝에서부터 다음 문자까지의 거리
-        const fromEnd = len - next;
+        // 방법1. 다음 문자(next)까지 왔던 길을 다시 돌아간 후, 문자열의 끝에서부터 접근하는 경우
+        const goRightThenLeft = cursor + cursor + (len - next);
+        // 방법2. 아예 끝에서부터 다음 문자(next)까지 이동하고 현재 cursor까지 복귀하는 경우
+        const goLeftThenRight = (len - next) + (len - next) + cursor;
         
-        // 다음 문자까지의 직진 거리, 우회 거리 중 최소값 선택
-        leftAndRight = Math.min(leftAndRight, cursor + (len - next) + Math.min(cursor, len - next));
+        leftAndRight = Math.min(leftAndRight, goRightThenLeft, goLeftThenRight);
     }
     
     return upAndDown + leftAndRight;
-}
+};
