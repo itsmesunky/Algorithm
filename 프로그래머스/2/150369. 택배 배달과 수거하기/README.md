3c0bba2583b116f4428824a4b7c91b3c588ae77b
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-09-29 22:23:22 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 8.65 ms, Memory: 39.3 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/README.md
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 택배 배달과 수거하기 - 150369 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150369?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150369) 
 
 ### 성능 요약
 
-메모리: 41 MB, 시간: 44.01 ms
+메모리: 39.3 MB, 시간: 8.65 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 29일 15:20:06
+2025년 09월 29일 22:23:19
 
 ### 문제 설명
 
--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.js
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.js
@@ -1,28 +1,18 @@
 const solution = (cap, n, deliveries, pickups) => {
     let answer = 0;
-    
-    let d = [...deliveries];
-    let p = [...pickups];
-    
+    let dRemain = 0;
+    let pRemain = 0;
+
     for (let i = n - 1; i >= 0; i--) {
-        if (d[i] === 0 && p[i] === 0) continue;
+        dRemain += deliveries[i];
+        pRemain += pickups[i];
 
-        let trips = 0; 
-        
-        while (d[i] > 0 || p[i] > 0) {
-            trips++; // 왕복 횟수 증가
-            
-            d[i] -= cap; 
-            p[i] -= cap;
-        }
-
-        answer += (i + 1) * 2 * trips;
-
-        if (i > 0) {
-            d[i - 1] += d[i]; // 남은 배달 여유분을 다음 집에 더해줌
-            p[i - 1] += p[i]; // 남은 수거 여유분을 다음 집에 더해줌
+        while (dRemain > 0 || pRemain > 0) {
+            dRemain -= cap;
+            pRemain -= cap;
+            answer += (i + 1) * 2;
         }
     }
-    
+
     return answer;
-}
+};
