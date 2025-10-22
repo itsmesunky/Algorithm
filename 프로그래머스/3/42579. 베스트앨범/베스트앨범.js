92a165523db9b5fcb001053e3efb18ec85a58993
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-10-22 17:08:16 +0900

    [level 3] Title: 베스트앨범, Time: 0.52 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,87 +1,3 @@
-class MaxHeap {
-    constructor() {
-        this.heap = [];
-    }
-    
-    getParentIndex(index) {
-        return Math.floor((index - 1) / 2);
-    }
-    
-    getLeftChildIndex(index) {
-        return index * 2 + 1;
-    }
-    
-    getRightChildIndex(index) {
-        return index * 2 + 2;
-    }
-    
-    swap(a, b) {
-        [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
-    }
-    
-    size() {
-        return this.heap.length;
-    }
-    
-    pop() {
-        if(this.heap.length === 0) return false;
-        if(this.heap.length === 1) return this.heap.pop();
-        
-        const root = this.heap[0];
-        this.heap[0] = this.heap.pop();
-        this.heapifyDown();
-        return root;
-    }
-    
-    push(arr) {
-        this.heap.push(arr);
-        this.heapifyUp();
-    }
-    
-    heapifyUp() {
-        let index = this.heap.length - 1;
-        
-        while(index > 0) {
-            const parent = this.getParentIndex(index);
-            
-            // 재생 수 비교
-            if(this.heap[index][1] < this.heap[parent][1]) break;
-            
-            // 재생수가 같은 경우 고유 번호 비교
-            if(this.heap[index][1] === this.heap[parent][1]
-               && this.heap[parent][0] < this.heap[index][0]) break;
-            
-            this.swap(index, parent);
-            index = parent;
-        }
-    }
-    
-    heapifyDown() {
-        let index = 0;
-        const length = this.heap.length;
-        
-        while(this.getLeftChildIndex(index) < length) {
-            const left = this.getLeftChildIndex(index);
-            const right = this.getRightChildIndex(index);
-            
-            let largger = left;
-            
-            if(right < length && this.heap[left][1] < this.heap[right][1]) {
-                largger = right;
-            }
-            
-            if(this.heap[largger][1] < this.heap[index][1]) break;
-            
-            // 재생 수가 같은 경우 고유번호로 비교
-            if(this.heap[largger][1] === this.heap[index[1]] 
-               && this.heap[index][0] < this.heap[largger][0]) break;
-            
-            this.swap(index, largger);
-            index = largger;
-        }
-    }
-}
-
 const solution = (genres, plays) => {
     const answer = [];
     const obj = {};
@@ -90,29 +6,22 @@ const solution = (genres, plays) => {
         const key = genre;
         const play = plays[i];
         
-        if(!obj[key]){
-            obj[key] = {
-                totalPlays: 0,
-                songs: new MaxHeap()
-            };
-        }
+        if(!obj[key]) obj[key] = { totalPlays: 0, songs: [] };
         
         obj[key].totalPlays += play;
         obj[key].songs.push([i, play]);
     })
     
+    // 장르별 총재생 수 기준 내림차순 정렬
     const values = Object.values(obj).sort((a, b) => b.totalPlays - a.totalPlays);
     
     for(const object of values) {
-        const heap = object.songs;
+        const arr = object.songs;
         
-        if(heap.size() === 1) {
-            answer.push(heap.pop()[0]);
-            continue;
-        }
-        
-        answer.push(heap.pop()[0]);
-        answer.push(heap.pop()[0]);
+        // 1. 재생수 기준 내림차순 정렬
+        // 2. 고유 번호 기준 오름차순 정렬
+        arr.sort((a, b) => b[1] - a[1] || a[0] - b[0]);
+        arr.slice(0, 2).forEach(([index, _]) => answer.push(index));
     }
     
     return answer;
