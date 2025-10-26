368eff0d352df6ddab15e9ed21507be4ba3146a6
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-10-26 17:59:19 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 30.33 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
@@ -1,24 +1,30 @@
-function solution(gems) {
-  const totalKinds = new Set(gems).size;
-  const counter = new Map();
-  let answer = [0, gems.length - 1];
-
-  let left = 0;
-  for (let right = 0; right < gems.length; right++) {
-    counter.set(gems[right], (counter.get(gems[right]) || 0) + 1);
-
-    while (counter.size === totalKinds) {
-      if (right - left < answer[1] - answer[0]) {
-        answer = [left, right];
-      }
-
-      // 왼쪽 보석 제거
-      const gem = gems[left];
-      counter.set(gem, counter.get(gem) - 1);
-      if (counter.get(gem) === 0) counter.delete(gem);
-      left++;
+const solution = (gems) => {
+    let answer = [0, gems.length - 1];
+    
+    // 보석 종류 수
+    const kinds = new Set(gems).size;
+    
+    // gems 배열 탐색하면서 찾은 보석별 개수
+    const counter = new Map();
+    
+    let left = 0;
+    for(let right = 0; right < gems.length; right++) {
+        const rKey = gems[right];
+        counter.set(rKey, (counter.get(rKey) || 0) + 1);
+        
+        // 모든 종류의 보석을 찾았을 때
+        while(counter.size === kinds) {
+            if(right - left < answer[1] - answer[0]) {
+                answer = [left, right];
+            }
+            
+            // 최소 범위 찾기
+            const lKey = gems[left];
+            counter.set(lKey, counter.get(lKey) - 1);
+            if(!counter.get(lKey)) counter.delete(lKey);
+            left++;
+        }
     }
-  }
-
-  return [answer[0] + 1, answer[1] + 1];
+    
+    return [answer[0] + 1, answer[1] + 1];
 }
