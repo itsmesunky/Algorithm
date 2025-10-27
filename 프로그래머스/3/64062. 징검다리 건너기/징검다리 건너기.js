423916db9f7d331ede36f30e26467612bb811b1e
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-10-27 11:06:43 +0900

    [level 3] Title: 징검다리 건너기, Time: 56.35 ms, Memory: 43.3 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.js
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.js
@@ -1,17 +1,22 @@
-function solution(stones, k) {
-    let left = 1;
-    let right = 200000000; // 문제 제한: 최대 2억 명까지 가능
+/**
+* 문제 해결 시나리오
+* 1. 최대 몇 명까지 징검다리를 건널 수 있는지 이분 탐색으로 찾으면 됨
+* 2. 최소 1명, 최대 2억명
+*/
+const solution = (stones, k) => {
     let answer = 0;
+    let left = 0;
+    let right = 200_000_000;
     
-    while (left <= right) {
-        const mid = Math.floor((left + right) / 2);
+    while(left <= right) {
+        let mid = Math.floor((left + right) / 2);
         let skip = 0;
         let canCross = true;
         
-        for (let stone of stones) {
-            if (stone - mid < 0) {
+        for(const stone of stones) {
+            if(stone - mid < 0) {
                 skip += 1;
-                if (skip >= k) {
+                if(k <= skip) {
                     canCross = false;
                     break;
                 }
@@ -20,12 +25,10 @@ function solution(stones, k) {
             }
         }
         
-        if (canCross) {
-            // mid 명이 건널 수 있음 → 더 많은 사람 가능성 탐색
-            answer = mid;
+        if(canCross) {
             left = mid + 1;
+            answer = mid;
         } else {
-            // mid 명은 불가능 → 더 적게 시도
             right = mid - 1;
         }
     }
