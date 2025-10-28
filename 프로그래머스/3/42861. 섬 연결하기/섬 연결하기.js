502f189e572eda59234dbb78e1872a2ce7efc6b9
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-10-28 10:00:52 +0900

    [level 3] Title: 섬 연결하기, Time: 0.39 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
@@ -1,46 +1,44 @@
 /**
 * 문제 해결 시나리오
-* 1. 최소 비용으로 모든 노드를 연결
-* 2. Kruskal + Union-Find 알고리즘 활용
+* 1. 최소 비용으로 모든 노드 연결 → Kruskal
+* 2. 단, 사이클을 만들지 않아야 함 → Union-Find
 */
 const solution = (n, costs) => {
-    // 각 노드의 부모 노드를 저장하기 위한 배열
     const parent = Array.from({length: n}, (_, idx) => idx);
     
-    // 부모 노드 찾기 - recursive
-    const getParent = (node) => {
-        const result = parent[node];
-        if(result === node) return result;
-        return parent[node] = getParent(result);
+    // 특정 노드의 부모 노드 찾기
+    const getParent = (x) => {
+        if(parent[x] === x) return x;
+        return parent[x] = getParent(parent[x]);
     }
     
-    // 부모의 값이 더 작은 쪽으로 부모 합침
-    const unionParent = (node1, node2) => {
-        const rootA = getParent(node1);
-        const rootB = getParent(node2);
+    // 부모 합침
+    const unionParent = (x, y) => {
+        const rootX = getParent(x);
+        const rootY = getParent(y);
         
-        if (rootA < rootB) {
-            parent[rootB] = rootA;
+        // 둘 중 더 작은 값으로 업데이트
+        if(rootX < rootY) {
+            parent[rootY] = rootX;
         } else {
-            parent[rootA] = rootB;
+            parent[rootX] = rootY;
         }
     }
     
-    // 같은 부모, 즉 사이클을 그리는지 확인
-    const isCycle = (a, b) => getParent(a) === getParent(b);
+    // 사이클 형성 여부
+    const isUnion = (x, y) => getParent(x) === getParent(y);
     
-    // 간선 비용 오름차순 정렬
+    // Kruskal 알고리즘을 활용을 위해 간선 비용 오름차순 정렬
     costs.sort((a, b) => a[2] - b[2]);
     
-    let answer = 0;
+    let sum = 0;
+    
     for(const [a, b, c] of costs) {
-        // 연결된 그래프인지 확인
-        if(isCycle(a, b)) continue;
-        // 연결 및 부모 합침
-        unionParent(a, b);
-        // 비용 누적
-        answer += c;
+        if(!isUnion(a, b)) {
+            unionParent(a, b); // 두 노드 연결
+            sum += c; // 비용 누적
+        }
     }
     
-    return answer;
+    return sum;
 }
