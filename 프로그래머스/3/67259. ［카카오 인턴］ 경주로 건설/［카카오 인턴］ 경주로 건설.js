b6cf208530d81b84c0e8fc414e73dce02cf3bb41
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-10-29 15:37:00 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 12.16 ms, Memory: 39.2 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.js
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.js
@@ -1,15 +1,14 @@
 const solution = (board) => {
-    let answer = Number.MAX_SAFE_INTEGER;
+    const R = board.length;
+    const C = board[0].length;
+    
+    const minCost = Array.from({ length: R }, () => 
+                               Array.from({ length: C }, () => 
+                                          Array(4).fill(Infinity)));
     
     const bfs = () => {
         // 시계 방향(우하좌상)으로 정의
         const dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]];
-        
-        const R = board.length;
-        const C = board[0].length;
-        const minCost = Array.from({length: R}, () => 
-                                   Array.from({length: C}, () =>
-                                             Array(4).fill(Infinity)));
         
         // [현재행, 현재열, 현재 방향, 누적 금액]
         const queue = [];
@@ -28,19 +27,12 @@ const solution = (board) => {
         while(head < queue.length) {
             const [currR, currC, currD, currCost] = queue[head++];
             
-            if(currR === R - 1 && currC === C - 1) {
-                answer = Math.min(currCost, answer);
-                continue;
-            }
-            
             for(let nextD = 0; nextD < 4; nextD++) {
                 const nr = currR + dirs[nextD][0];
                 const nc = currC + dirs[nextD][1];
                 
-                // 테두리, 벽 감지 및 방문 여부 확인
-                if(nr < 0 || nc < 0 || nr >= R || nc >= C || board[nr][nc]) {
-                    continue;
-                }
+                // 테두리, 벽 감지
+                if(nr < 0 || nc < 0 || nr >= R || nc >= C || board[nr][nc]) continue;
                 
                 const cornerCost = (currD === nextD) ? 0 : 500;
                 const newCost = currCost + 100 + cornerCost;
@@ -55,5 +47,5 @@ const solution = (board) => {
     
     bfs();
     
-    return answer;
+    return Math.min(...minCost[R - 1][C - 1])
 }
