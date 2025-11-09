2b21ee2f89d057575530f957f62861f8d61c23c9
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-11-09 15:54:25 +0900

    [level 3] Title: 표 편집, Time: 204.69 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.js
+++ 프로그래머스/3/81303. 표 편집/표 편집.js
@@ -1,120 +1,52 @@
-class Row {
-  constructor(index) {
-    this.index = index;
-    this.prev = null;
-    this.next = null;
-  }
-}
-
-class Table {
-  constructor(n, k) {
-    this.rows = [];
-    this.cursor = null;
-    this.deletedStack = [];
-    this.initTable(n, k);
-  }
-
-  initTable(n, k) {
-    for (let i = 0; i < n; i++) {
-      this.rows[i] = new Row(i);
+const solution = (n, k, cmd) => {
+    // 행 삭제 여부
+    const isRemoved = Array(n).fill(false);
+    // 삭제 행 스택
+    const deletedStack = [];
+    
+    // 이중 연결 리스트
+    // 각 행의 이전/다음 행 번호 저장 배열
+    const prevRow = Array(n);
+    const nextRow = Array(n);
+    for(let i = 0; i < n; i++) {
+        prevRow[i] = i - 1;
+        nextRow[i] = i === n - 1 ? -1 : i + 1;
     }
-
-    for (let i = 0; i < n; i++) {
-      if (i > 0) this.rows[i].prev = this.rows[i - 1];
-      if (i < n - 1) this.rows[i].next = this.rows[i + 1];
+    
+    // 현재 선택 행
+    let selectedRow = k;
+    
+    for(const command of cmd) {
+        const [operation, num] = command.split(" ");
+        let parseNum = Number(num);
+        
+        switch(operation) {
+            case "U":
+                while(parseNum--) selectedRow = prevRow[selectedRow];
+                break;
+            case "D":
+                while(parseNum--) selectedRow = nextRow[selectedRow];
+                break;
+            case "C":
+                // 이전/다음 노드 연결 끊기
+                const prev = prevRow[selectedRow];
+                const next = nextRow[selectedRow];
+                if(prev !== -1) nextRow[prev] = next;
+                if(next !== -1) prevRow[next] = prev;
+                
+                isRemoved[selectedRow] = true;
+                deletedStack.push(selectedRow);
+                
+                selectedRow = nextRow[selectedRow] !== -1 ? nextRow[selectedRow] : prevRow[selectedRow];
+                break;
+            case "Z":
+                const restore = deletedStack.pop();
+                if(prevRow[restore] !== -1) nextRow[prevRow[restore]] = restore;
+                if(nextRow[restore] !== -1) prevRow[nextRow[restore]] = restore;
+                isRemoved[restore] = false;
+                break;
+        }
     }
-
-    this.cursor = this.rows[k];
-  }
-
-  up(moveCount) {
-    for (let i = 0; i < moveCount; i++) {
-      this.cursor = this.cursor.prev;
-    }
-  }
-
-  down(moveCount) {
-    for (let i = 0; i < moveCount; i++) {
-      this.cursor = this.cursor.next;
-    }
-  }
-
-  remove() {
-    const deletedRow = this.cursor;
-
-    // 삭제할 때 prev/next 정보를 저장해두기
-    this.deletedStack.push({
-      row: deletedRow,
-      prevRow: deletedRow.prev,
-      nextRow: deletedRow.next,
-    });
-
-    // 연결 끊기
-    if (deletedRow.prev) {
-      deletedRow.prev.next = deletedRow.next;
-    }
-    if (deletedRow.next) {
-      deletedRow.next.prev = deletedRow.prev;
-    }
-
-    // 커서 이동
-    if (deletedRow.next) {
-      this.cursor = deletedRow.next;
-    } else {
-      this.cursor = deletedRow.prev;
-    }
-  }
-
-  restore() {
-    if (this.deletedStack.length === 0) return;
-
-    const { row: restoredRow, prevRow, nextRow } = this.deletedStack.pop();
-
-    // 저장된 연결 정보로 즉시 복구 (O(1))
-    restoredRow.prev = prevRow;
-    restoredRow.next = nextRow;
-
-    if (prevRow) {
-      prevRow.next = restoredRow;
-    }
-    if (nextRow) {
-      nextRow.prev = restoredRow;
-    }
-  }
-
-  getResult() {
-    const result = new Array(this.rows.length).fill('O');
-
-    for (const { row } of this.deletedStack) {
-      result[row.index] = 'X';
-    }
-
-    return result.join('');
-  }
-}
-
-function solution(n, k, cmd) {
-  const table = new Table(n, k);
-
-  for (let i = 0; i < cmd.length; i++) {
-    const [command, x] = cmd[i].split(' ');
-
-    switch (command) {
-      case 'U':
-        table.up(parseInt(x));
-        break;
-      case 'D':
-        table.down(parseInt(x));
-        break;
-      case 'C':
-        table.remove();
-        break;
-      case 'Z':
-        table.restore();
-        break;
-    }
-  }
-
-  const result = table.getResult();
-  return result;
+    
+    return isRemoved.map(bool => bool ? "X" : "O").join('');
 }
