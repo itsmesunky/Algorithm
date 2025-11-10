db1950a59935646ce1400c2499191ffaad19b570
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-11-10 12:00:13 +0900

    [level 3] Title: 양과 늑대, Time: 29.27 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.js
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.js
@@ -1,35 +1,33 @@
 const solution = (info, edges) => {
     const graph = Array.from({ length: info.length }, () => []);
-    for (const [a, b] of edges) graph[a].push(b);
-
-    let maxSheep = 0;
-
+    for(const [parent, child] of edges) {
+        graph[parent].push(child);
+    }
+    
+    let maxSheepCount = 0;
+    
     const dfs = (sheep, wolf, node, nextNodes) => {
-        // 방문할 노드가 양인지 늑대인지 처리
-        if (info[node] === 0) sheep++;
+        if(info[node] === 0) sheep++;
         else wolf++;
-
-        // 늑대가 양 이상이면 더 진행 불가
-        if (wolf >= sheep) return;
-
-        // 최대 양 갱신
-        maxSheep = Math.max(maxSheep, sheep);
-
-        // nextNodes 업데이트
-        const newNextNodes = new Set(nextNodes);
-        newNextNodes.delete(node);
-
-        for (const child of graph[node]) {
-            newNextNodes.add(child);
+        
+        maxSheepCount = Math.max(maxSheepCount, sheep);
+        
+        if(sheep === wolf) return;
+        
+        const newNodes = new Set(nextNodes);
+        newNodes.delete(node);
+        
+        for(const child of graph[node]) {
+            newNodes.add(child);
         }
-
-        // 현재 갈 수 있는 모든 후보 노드 탐색
-        for (const next of newNextNodes) {
-            dfs(sheep, wolf, next, newNextNodes);
+        
+        // 현재 노드에서 이동 가능한 후보 노드들에 대해 DFS
+        for(const candidate of newNodes) {
+            dfs(sheep, wolf, candidate, newNodes);
         }
-    };
-
+    }
+    
     dfs(0, 0, 0, new Set([0]));
-
-    return maxSheep;
-};
+    
+    return maxSheepCount;
+}
