6a878e432ac335029ff011569ab5903942d4bf51
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-11-11 14:18:50 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 11.15 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.js
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.js
@@ -1,36 +1,52 @@
-function solution(n, m, x, y, r, c, k) {
-    const dist = Math.abs(r - x) + Math.abs(c - y);
-    if (dist > k || (k - dist) % 2 === 1) return "impossible";
+/**
+* getManhattanDist - 두 좌표 간 맨해튼 거리를 반환하는 함수
+* @param {number} ax - A의 x 좌표
+* @param {number} bx - B의 x 좌표
+* @param {number} ay - A의 y 좌표
+* @param {number} by - B의 y 좌표
+* @returns {number} - 두 좌표 간 맨해튼 거리
+*/
+const getManhattanDist = (ax, bx, ay, by) => Math.abs(bx - ax) + Math.abs(by - ay);
 
-    let answer = "";
-    let cx = x, cy = y;
-
+const solution = (n, m, x, y, r, c, k) => {
+    const manhattanDist = getManhattanDist(y, c, x, r);
+    
+    // 조기 종료
+    // k에서 맨해튼 거리를 제외한 값이 짝수가 아니라면 불가(패리티 검사)
+    if(k < manhattanDist || (k - manhattanDist) % 2 === 1) return "impossible";
+    
+    let answer = '';
+    
+    // 방향 정의
+    // 애초에 방향 정의를 오름차순으로 작성
     const dirs = [
         ['d', 1, 0],
         ['l', 0, -1],
         ['r', 0, 1],
         ['u', -1, 0],
     ];
-
-    for (let step = 0; step < k; step++) {
-        for (const [ch, dx, dy] of dirs) {
-            const nx = cx + dx;
-            const ny = cy + dy;
-
-            if (nx < 1 || ny < 1 || nx > n || ny > m) continue;
-
-            const remaining = k - (step + 1);
-            const distToTarget = Math.abs(r - nx) + Math.abs(c - ny);
-
-            if (remaining < distToTarget) continue;
-            if ((remaining - distToTarget) % 2 !== 0) continue;
-
-            answer += ch;
-            cx = nx;
-            cy = ny;
+    
+    // 현재 위치 행/열
+    let currR = x, currC = y;
+    
+    for(let move = 1; move <= k; move++){
+        for(const [command, dr, dc] of dirs) {
+            const nr = currR + dr;
+            const nc = currC + dc;
+            
+            if(nr < 1 || nc < 1 || nr > n || nc > m) continue;
+            
+            const remaining = k - move;
+            const currManhattanDist = getManhattanDist(nc, c, nr, r);
+            if(remaining < currManhattanDist) continue;
+            if((remaining - currManhattanDist) % 2 !== 0) continue;
+            
+            currR = nr;
+            currC = nc;
+            answer += command;
             break;
         }
     }
-
+    
     return answer;
 }
