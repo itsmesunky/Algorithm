227351c1c32a8a9192ad5f510509d86f7b644769
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-11-13 14:39:54 +0900

    [level 3] Title: 110 옮기기, Time: 153.66 ms, Memory: 46.5 MB -BaekjoonHub

--- 프로그래머스/3/77886. 110 옮기기/110 옮기기.js
+++ 프로그래머스/3/77886. 110 옮기기/110 옮기기.js
@@ -1,44 +1,36 @@
-function solution(s) {
-  const answer = [];
+const solution = (s) => {
+    return s.map((str) => {
+        // 조기 종료 조건
+        if(str.length < 4) return str;
+        if(str.indexOf('110') === -1) return str;
+        
+        const stack = [];
+        let count = 0;
+        for(const char of str) {
+            if(stack.length >= 2) {
+                if(char === '0' && stack.at(-1) === '1' && stack.at(-2) === '1') {
+                    count++;
+                    for(let i = 0; i < 2; i++) stack.pop();
+                } else {
+                    stack.push(char);
+                }
+            } else {
+                stack.push(char);
+            }
+        }
+        
+        let result = stack.join('');
+        const composite = '110'.repeat(count);
+        const lastZeroIndex = result.lastIndexOf('0');
 
-  for (const str of s) {
-    const stack = [];
-    let count110 = 0;
+        if(lastZeroIndex === -1) { // 문자열 내에 0이 없는 경우
+            result = composite + result;
+        } else {
+            const prev = result.slice(0, lastZeroIndex + 1);
+            const after = result.slice(lastZeroIndex + 1);
+            result = prev + '110'.repeat(count) + after;
+        }
 
-    // 1. 모든 "110"을 빼내고 개수 세기
-    for (const ch of str) {
-      stack.push(ch);
-
-      if (
-        stack.length >= 3 &&
-        stack[stack.length - 3] === '1' &&
-        stack[stack.length - 2] === '1' &&
-        stack[stack.length - 1] === '0'
-      ) {
-        stack.pop();
-        stack.pop();
-        stack.pop();
-        count110++;
-      }
-    }
-
-    // 2. 남은 문자열에서 "110"을 끼워넣을 위치 찾기
-    const base = stack.join('');
-    const insert = '110'.repeat(count110);
-
-    // 2-1. 남은 문자열에서 가장 마지막 '0'의 위치
-    const lastZeroIdx = base.lastIndexOf('0');
-
-    // 2-2. '0'이 하나도 없으면 맨 앞에 다 집어넣기
-    if (lastZeroIdx === -1) {
-      answer.push(insert + base);
-    } else {
-      // 2-3. 마지막 '0' 바로 뒤에 "110"들 전부 삽입
-      const left = base.slice(0, lastZeroIdx + 1);
-      const right = base.slice(lastZeroIdx + 1);
-      answer.push(left + insert + right);
-    }
-  }
-
-  return answer;
+        return result;
+    });
 }
