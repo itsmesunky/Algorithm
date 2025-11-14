e84e9170d7e78b7949ca83866f791394b57e7a0e
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-11-14 16:16:28 +0900

    [Silver I] Title: 미로 탐색, Time: 172 ms, Memory: 12892 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.js
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.js
@@ -1,34 +1,34 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const [R, C] = input.shift().split(' ').map(Number); 
-const grid = input.map(row => row.split('').map(Number)); 
+const [R, C] = input.shift().split(' ').map(Number);
+const grid = input.map(row => row.split('').map(Number));
 
 const dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]];
 
 const BFS = () => {
-    const visited = Array.from({ length: R }, () => Array(C).fill(false));
-    
-    visited[0][0] = true;
-    let head = 0;
-    const queue = [[0, 0, 1]]; 
+    const visited = Array.from({ length: R + 1 }, () => Array(C + 1).fill(false));
+    visited[1][1] = true;
 
-    while(head < queue.length) {
-        const [currR, currC, moves] = queue[head++];
-        
-        if(currR === R - 1 && currC === C - 1) return moves; 
+    let head = 0;
+    const queue = [[1, 1, 1]]; // [현재 행, 현재 열, 이동 횟수]
+    while(head < queue.length) {
+        const [currR, currC, moves] = queue[head++];
+        
+        if(currR === R && currC === C) return moves;
 
-        for(let i = 0; i < 4; i++) {
-            const nr = currR + dirs[i][0];
-            const nc = currC + dirs[i][1];
+        for(let i = 0; i < 4; i++) {
+            const nr = currR + dirs[i][0];
+            const nc = currC + dirs[i][1];
 
-            if(nr < 0 || R <= nr || nc < 0 || C <= nc) continue;
+            // 테두리
+            if(nr < 1 || R < nr || nc < 1 || C < nc) continue;
 
-            if(!visited[nr][nc] && grid[nr][nc] === 1) { 
-                visited[nr][nc] = true; // 다음 칸을 방문 처리
-                queue.push([nr, nc, moves + 1]);
-            }
-        }
-    }
+            if(!visited[nr][nc] && grid[nr - 1][nc - 1] === 1) {
+                visited[nr][nc] = true;
+                queue.push([nr, nc, moves + 1]);
+            }
+        }
+    }
 }
 
 console.log(BFS());
