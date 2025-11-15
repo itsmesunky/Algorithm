e09380ce48f7205b359399a19dd4b65ac848f49f
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-11-15 14:54:22 +0900

    [Silver IV] Title: 동전 0, Time: 96 ms, Memory: 9592 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.js
+++ 백준/Silver/11047. 동전 0/동전 0.js
@@ -3,8 +3,11 @@ const input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
 // 준규가 가지고 있는 동전 종류 수, 만들고자 하는 값
 const [N, K] = input[0].split(' ').map(Number);
 
+// 동전 가치 배열
+const coins = input.slice(1).map(Number);
+
 // 동전 가치를 가리킬 포인터
-let pointer = N;
+let pointer = N - 1;
 
 // 현재 남은 값
 let rest = K;
@@ -12,10 +15,10 @@ let rest = K;
 let answer = 0;
 
 while(rest > 0) {
-    const quotient = Math.floor(rest / input[pointer]);
+    const quotient = Math.floor(rest / coins[pointer]);
     if(quotient) { // 현재 동전 가치로 남은 값을 만들 수 있는 경우
         answer += quotient;
-        rest %= input[pointer];
+        rest %= coins[pointer];
     }
     
     pointer--;
