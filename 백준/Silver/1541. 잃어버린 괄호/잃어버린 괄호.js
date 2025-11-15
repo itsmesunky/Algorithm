42b24a20a5a1f1085cfefcd38b3291b7aedb41cd
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-11-15 22:35:52 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 92 ms, Memory: 9348 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.js
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.js
@@ -1,10 +1,19 @@
+/**
+ * 문제 해결 시나리오
+ * - 목표: 식 내 특정 부분을 괄호로 묶어서 최솟값 도출해내기
+ * - 해결방안
+ *   - '-' 뒤에 위치하는 피연산자의 값을 괄호로 묶어 최댓값으로 만들고 그 값을 빼면 됨
+ */
 const input = require('fs').readFileSync('/dev/stdin').toString().trim();
 
-const groups = input.split('-').map(g => g.split('+').reduce((a,b)=>a+Number(b),0));
+const numbers = input
+  .split('-')
+  .map((str) => str.split('+').reduce((acc, cur) => acc + parseInt(cur), 0));
 
-let answer = groups[0];
-for (let i = 1; i < groups.length; i++) {
-  answer -= groups[i];
+let result = numbers[0];
+
+for (let i = 1; i < numbers.length; i++) {
+  result -= numbers[i];
 }
 
-console.log(answer);
+console.log(result);
