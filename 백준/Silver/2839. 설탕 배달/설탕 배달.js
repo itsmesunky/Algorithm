392e2526a32f4753d914a9af0544790907d0551a
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-11-15 12:48:31 +0900

    [Silver IV] Title: 설탕 배달, Time: 96 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.js
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.js
@@ -1,20 +1,16 @@
-const input = +require("fs").readFileSync("/dev/stdin").toString().trim();
+const input = require("fs").readFileSync("/dev/stdin").toString().trim();
 
-const dp = Array(input + 1).fill(-1);
-dp[3] = 1;
-dp[5] = 1;
-
-for(let i = 6; i <= input; i++) {
-    const a = dp[i - 3] + 1;
-    const b = dp[i - 5] + 1;
-    
-    if(!a && !b) {
-        dp[i] = -1;
-    } else if(!a || !b) {
-        dp[i] = Math.max(a, b);
-    } else {
-        dp[i] = Math.min(a, b);
+let N = parseInt(input);
+let answer = 0;
+while(N >= 0) {
+    if(N % 5 === 0) {
+        answer += N / 5;
+        console.log(answer);
+        return;
     }
+    
+    N -= 3;
+    answer++;
 }
 
-console.log(dp[input]);
+console.log(-1);
