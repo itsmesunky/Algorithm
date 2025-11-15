5d6a5bf4b78676b9c0eab87f674666292095a363
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-11-15 23:07:34 +0900

    [Silver IV] Title: 보물, Time: 96 ms, Memory: 9600 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.js
@@ -0,0 +1,21 @@
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+const N = parseInt(input[0]);
+
+const A = input[1].split(' ').map(Number);
+const B = input[2].split(' ').map(Number);
+
+A.sort((a, b) => a - b);
+B.sort((a, b) => b - a);
+
+let result = 0;
+
+for (let i = 0; i < N; i++) {
+  result += A[i] * B[i];
+}
+
+console.log(result);
