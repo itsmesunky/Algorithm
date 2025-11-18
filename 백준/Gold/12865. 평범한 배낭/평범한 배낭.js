d829136d4b0c3a3c4e1ffa22f6e16c6221c2bf33
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-11-18 12:14:59 +0900

    [Gold V] Title: 평범한 배낭, Time: 208 ms, Memory: 10648 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.js
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.js
@@ -1,19 +1,19 @@
-const input = require('fs')
-  .readFileSync('/dev/stdin')
+const input = require("fs")
+  .readFileSync("/dev/stdin")
   .toString()
   .trim()
-  .split('\n');
+  .split("\n");
 
-const [N, K] = input[0].split(" ").map(Number);
-const items = input.slice(1).map(str => str.split(" ").map(Number));
+const [N, K] = input[0].split(" ").map(Number); // 물품 수, 배낭 무게
+const items = input.slice(1).map((str) => str.split(" ").map(Number));
 
 const dp = Array(K + 1).fill(0);
 
 for (let i = 0; i < N; i++) {
-  const [w, v] = items[i];
+  const [weight, value] = items[i];
 
-  for (let weight = K; weight >= w; weight--) {
-    dp[weight] = Math.max(dp[weight], dp[weight - w] + v);
+  for (let j = K; j >= weight; j--) {
+    dp[j] = Math.max(dp[j], dp[j - weight] + value);
   }
 }
 
