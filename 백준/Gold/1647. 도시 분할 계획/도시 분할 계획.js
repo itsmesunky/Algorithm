022f05d9da936ec1c0f5776f27a04b53b1aa740c
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-12-02 17:02:07 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2360 ms, Memory: 241752 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.js
@@ -0,0 +1,41 @@
+const input = require("fs")
+  .readFileSync(0, "utf-8")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [V, E] = input.shift().split(" ").map(Number);
+const infos = input.map((str) => str.split(" ").map(Number));
+
+// kruskal 알고리즘 활용을 위한 간선 비용 오름차순 정렬
+infos.sort((a, b) => a[2] - b[2]);
+
+const parent = Array.from({ length: V + 1 }, (_, i) => (0 < i ? i : Infinity));
+
+const getParent = (x) => {
+  if (parent[x] === x) return x;
+  return (parent[x] = getParent(parent[x]));
+};
+
+const union = (x, y) => {
+  const rootX = getParent(x);
+  const rootY = getParent(y);
+
+  if (rootX < rootY) parent[rootY] = rootX;
+  else parent[rootX] = rootY;
+};
+
+// 그래프를 형성하지 않도록 함
+const isUnion = (x, y) => getParent(x) === getParent(y);
+
+let totalCost = 0;
+let maxCost = 0;
+for (const [A, B, C] of infos) {
+  if (!isUnion(A, B)) {
+    union(A, B);
+    totalCost += C;
+    maxCost = C;
+  }
+}
+
+console.log(totalCost - maxCost);
