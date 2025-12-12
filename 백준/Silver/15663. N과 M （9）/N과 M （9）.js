081e1e729b574fa509e1fed97c0b1d6645fd06e5
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-12-12 14:14:10 +0900

    [Silver II] Title: N과 M (9), Time: 988 ms, Memory: 17840 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.js
@@ -0,0 +1,41 @@
+const input = require("fs")
+  .readFileSync(0, "utf-8")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [N, M] = input.shift().split(" ").map(Number);
+
+const numbers = input[0]
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
+
+const visited = Array(N).fill(false);
+
+const set = new Set();
+
+function DFS(arr) {
+  if (arr.length === M) {
+    set.add(arr.join(" "));
+    return;
+  }
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (!visited[i]) {
+      visited[i] = true;
+      arr.push(numbers[i]);
+      DFS(arr);
+      // 백트래킹
+      arr.pop();
+      visited[i] = false;
+    }
+  }
+}
+
+DFS([]);
+
+const result = [...set];
+for (const str of result) {
+  console.log(...str.split(" ").map(Number));
+}
