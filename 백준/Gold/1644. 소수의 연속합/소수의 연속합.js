4899de28d58033e95672c70bbf16bb1013466f3e
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-12-16 15:00:53 +0900

    [Gold III] Title: 소수의 연속합, Time: 292 ms, Memory: 50316 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.js
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.js
@@ -1,42 +1,44 @@
 const fs = require("fs");
-const input = fs.readFileSync("dev/stdin").toString().trim().split("\n");
+const input = fs.readFileSync("/dev/stdin").toString().trim();
 
-const N = parseInt(input[0]);
+const N = parseInt(input);
 
-function isPrime(num) {
-  const sqrt = Math.sqrt(num);
-  for (let i = 2; i <= sqrt; i++) {
-    if (num % i === 0) return false;
+// 1. 에라토스테네스의 체로 소수 구하기 (핵심!)
+const isPrime = new Array(N + 1).fill(true);
+isPrime[0] = false;
+isPrime[1] = false;
+
+for (let i = 2; i * i <= N; i++) {
+  if (isPrime[i]) {
+    // i의 배수들을 모두 지움 (i*i부터 시작해도 됨)
+    for (let j = i * i; j <= N; j += i) {
+      isPrime[j] = false;
+    }
   }
-  return true;
 }
 
-function getPrime() {
-  const primes = [0, 2, 3];
-  for (let num = 4; num <= N; num++) {
-    if (isPrime(num)) primes.push(num);
-  }
-  return primes;
+// 소수만 따로 모으기
+const primes = [];
+for (let i = 2; i <= N; i++) {
+  if (isPrime[i]) primes.push(i);
 }
 
-const prefixSum = getPrime();
-for (let i = 1; i < prefixSum.length; i++) {
-  prefixSum[i] += prefixSum[i - 1];
-}
-
+// 2. 투 포인터 (누적합 배열 없이 sum 변수로 관리)
 let answer = 0;
-let lt = 1;
-let rt = 1;
+let lt = 0;
+let rt = 0;
+let sum = 0;
 
-while (lt <= rt && rt < prefixSum.length) {
-  const sum = prefixSum[rt] - prefixSum[lt - 1];
-  if (sum === N) {
-    answer++;
+while (true) {
+  if (sum >= N) {
+    if (sum === N) answer++; // 정답 찾음
+    sum -= primes[lt]; // 왼쪽 포인터 이동 (값을 뺌)
     lt++;
-  } else if (sum < N) {
-    rt++;
+  } else if (rt === primes.length) {
+    break; // rt가 끝에 도달했고 sum < N이면 더 이상 가망 없음
   } else {
-    lt++;
+    sum += primes[rt]; // 오른쪽 포인터 이동 (값을 더함)
+    rt++;
   }
 }
 
