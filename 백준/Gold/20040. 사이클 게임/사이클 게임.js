a2d6a88709f5fcb2d019af6dc2afa3e2fa24b727
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-12-18 11:51:04 +0900

    [Gold IV] Title: 사이클 게임, Time: 1548 ms, Memory: 219144 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const [n, m] = input.shift().split(" ").map(Number);
+const infos = input.map((str) => str.split(" ").map(Number));
+
+const parent = Array.from({ length: n }, (_, i) => i);
+
+function getParent(x) {
+  if (parent[x] === x) return x;
+  else return getParent(parent[x]);
+}
+
+function unionFind(x, y) {
+  const rootX = getParent(x);
+  const rootY = getParent(y);
+
+  if (rootX === rootY) return true;
+
+  if (rootX < rootY) parent[rootY] = rootX;
+  else if (rootY < rootX) parent[rootX] = rootY;
+}
+
+for (let i = 0; i < m; i++) {
+  const [dot1, dot2] = infos[i];
+
+  if (unionFind(dot1, dot2)) {
+    console.log(i + 1);
+    return;
+  }
+}
+
+console.log(0);
