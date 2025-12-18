c4e460e1a11733773ae8cb8470eef41e8b311852
선우 <99164690+itsmesunky@users.noreply.github.com>
2025-12-18 10:53:16 +0900

    [Gold IV] Title: RGB거리 2, Time: 100 ms, Memory: 10212 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.js
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.js
@@ -1,32 +1,31 @@
 const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = fs.readFileSync("dev/stdin").toString().trim().split("\n");
 
 const N = parseInt(input.shift());
-const cost = input.map((str) => str.split(" ").map(Number));
+const costs = input.map((str) => str.split(" ").map(Number));
 
-const INF = 1000 * 1000 + 1;
-let answer = INF;
+const COLORS = 3;
 
-for (let firstColor = 0; firstColor < 3; firstColor++) {
-  const dp = Array.from({ length: N }, () => Array(3).fill(0));
+let answer = Infinity;
 
-  for (let i = 0; i < 3; i++) {
-    if (i === firstColor) {
-      dp[0][i] = cost[0][i];
-    } else {
-      dp[0][i] = INF;
-    }
+for (let firstColor = 0; firstColor < COLORS; firstColor++) {
+  const DP = Array.from({ length: N }, () => Array(COLORS).fill(0));
+
+  // 첫 번째 집의 고정 색 제외 선택 못하도록 나머지 색 비용 Infinity 처리
+  for (let i = 0; i < COLORS; i++) {
+    if (firstColor === i) DP[0][i] = costs[0][i];
+    else DP[0][i] = Infinity;
   }
 
-  for (let i = 1; i < N; i++) {
-    dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
-    dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
-    dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
+  for (let row = 1; row < N; row++) {
+    DP[row][0] = Math.min(DP[row - 1][1], DP[row - 1][2]) + costs[row][0];
+    DP[row][1] = Math.min(DP[row - 1][0], DP[row - 1][2]) + costs[row][1];
+    DP[row][2] = Math.min(DP[row - 1][0], DP[row - 1][1]) + costs[row][2];
   }
 
-  for (let lastColor = 0; lastColor < 3; lastColor++) {
-    if (firstColor === lastColor) continue; // 첫 집 색과 같으면 패스
-    answer = Math.min(answer, dp[N - 1][lastColor]);
+  for (let lastColor = 0; lastColor < COLORS; lastColor++) {
+    if (firstColor === lastColor) continue;
+    answer = Math.min(answer, DP[N - 1][lastColor]);
   }
 }
 
