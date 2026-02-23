526ddc55578e0f2b1c7c8538e95f2261958162c9
선우 <99164690+itsmesunky@users.noreply.github.com>
2026-02-23 13:32:11 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1512 ms, Memory: 312396 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.js
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.js
@@ -2,7 +2,12 @@ const fs = require("fs");
 const input = fs.readFileSync("dev/stdin").toString().trim().split("\n");
 
 const [N, M] = input.shift().split(" ").map(Number);
-const infos = input.map((str) => str.replace("\r", "").split("").map(Number));
+
+const board = input.map((str) => str.replace("\r\n", "").split("").map(Number));
+
+const visited = Array.from({ length: N }, () =>
+  Array.from({ length: M }, () => Array(2).fill(0)),
+);
 
 const dirs = [
   [-1, 0],
@@ -11,47 +16,41 @@ const dirs = [
   [0, 1],
 ];
 
-let answer = Infinity;
-const visited = Array.from({ length: N }, () =>
-  Array.from({ length: M }, () => Array(2).fill(0))
-);
-
 let head = 0;
 const queue = [[0, 0, 0]];
 visited[0][0][0] = 1;
 
 while (head < queue.length) {
   const [currRow, currCol, isBroken] = queue[head++];
-
   if (currRow === N - 1 && currCol === M - 1) {
-    answer = visited[currRow][currCol][isBroken];
-    break;
+    console.log(visited[currRow][currCol][isBroken]);
+    process.exit();
   }
 
-  for (let dir = 0; dir < 4; dir++) {
-    const nextRow = currRow + dirs[dir][0];
-    const nextCol = currCol + dirs[dir][1];
+  for (const [dx, dy] of dirs) {
+    const nextRow = currRow + dx;
+    const nextCol = currCol + dy;
 
-    if (nextRow < 0 || N <= nextRow || nextCol < 0 || M <= nextCol) continue;
+    if (N <= nextRow || nextRow < 0 || M <= nextCol || nextCol < 0) continue;
 
-    if (
-      infos[nextRow][nextCol] === 0 &&
-      visited[nextRow][nextCol][isBroken] === 0
-    ) {
+    const isWall = board[nextRow][nextCol] === 1;
+
+    // 조건 1 - 인접 노드가 벽인 경우
+    // 조건 2 - 해당 노드를 방문하지 않은 경우
+    // 조건 3 - 현재까지 벽을 부수지 않은 경우
+    if (isWall && !isBroken && !visited[nextRow][nextCol][1]) {
+      visited[nextRow][nextCol][1] = visited[currRow][currCol][0] + 1;
+      queue.push([nextRow, nextCol, 1]);
+    }
+
+    // 조건 1 - 인접 노드가 벽이 아닌 경우
+    // 조건 2 - 해당 노드를 방문하지 않은 경우
+    if (!isWall && !visited[nextRow][nextCol][isBroken]) {
       visited[nextRow][nextCol][isBroken] =
         visited[currRow][currCol][isBroken] + 1;
       queue.push([nextRow, nextCol, isBroken]);
     }
-
-    if (
-      infos[nextRow][nextCol] === 1 &&
-      isBroken === 0 &&
-      visited[nextRow][nextCol][1] === 0
-    ) {
-      visited[nextRow][nextCol][1] = visited[currRow][currCol][isBroken] + 1;
-      queue.push([nextRow, nextCol, 1]);
-    }
   }
 }
 
-console.log(answer === Infinity ? -1 : answer);
+console.log(-1);
