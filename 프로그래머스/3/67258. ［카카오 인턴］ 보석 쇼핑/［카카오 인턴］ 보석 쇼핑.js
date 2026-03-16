c0ec8bd62f93e5e223f99a77cbd797f81d0ddd48
선우 <99164690+itsmesunky@users.noreply.github.com>
2026-03-16 17:24:07 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 41.69 ms, Memory: 39.1 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
@@ -1,10 +1,8 @@
 const solution = (gems) => {
     let answer = [0, gems.length - 1];
     
-    // 보석 종류 수
     const kinds = new Set(gems).size;
     
-    // gems 배열 탐색하면서 찾은 보석별 개수
     const counter = new Map();
     
     let left = 0;
@@ -12,13 +10,11 @@ const solution = (gems) => {
         const rKey = gems[right];
         counter.set(rKey, (counter.get(rKey) || 0) + 1);
         
-        // 모든 종류의 보석을 찾았을 때
         while(counter.size === kinds) {
             if(right - left < answer[1] - answer[0]) {
                 answer = [left, right];
             }
             
-            // 최소 범위 찾기
             const lKey = gems[left];
             counter.set(lKey, counter.get(lKey) - 1);
             if(!counter.get(lKey)) counter.delete(lKey);
