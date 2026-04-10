fe0a8dd3e45d64961266200e135e6b3968b6e776
선우 <99164690+itsmesunky@users.noreply.github.com>
2026-04-10 14:57:08 +0900

    [level 3] Title: 합승 택시 요금, Time: 58.18 ms, Memory: 45 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/README.md
+++ 프로그래머스/3/72413. 합승 택시 요금/README.md
@@ -1,10 +1,10 @@
 # [level 3] 합승 택시 요금 - 72413 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72413) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72413?language=javascript) 
 
 ### 성능 요약
 
-메모리: 44.8 MB, 시간: 86.22 ms
+메모리: 45 MB, 시간: 58.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 07일 14:00:42
+2026년 04월 10일 14:57:05
 
 ### 문제 설명
 
--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.js
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.js
@@ -1,28 +1,26 @@
-/**
-* 문제 해결 시나리오
-* 1. 어느 정점에서 찢어져서 타야 최소 비용으로 갈 수 있는지 계산하면 됨
-* 2. 위의 식을 계산하기 위해 모든 노드에서 모든 노드까지의 최소 비용을 구하는 플로이드-워셜 알고리즘 호라용
-*/
 const solution = (n, s, a, b, fares) => {
     const floyd = Array.from({ length: n + 1 }, () => Array(n + 1).fill(Infinity));
-    floyd.forEach((arr, i) => arr[i] = 0);
     
-    for(const [s, e, value] of fares) {
-        floyd[s][e] = value;
-        floyd[e][s] = value;
+    for(let i = 1; i <= n; i++) {
+        floyd[i][i] = 0;
     }
     
-    for(let k = 1; k <= n; k++) {
-        for(let i = 1; i <= n; i++) {
-            for(let j = 1; j <= n; j++) {
-                if(floyd[i][k] + floyd[k][j] < floyd[i][j]) {
-                    floyd[i][j] = floyd[i][k] + floyd[k][j];
+    for(const [start, end, cost] of fares) {
+        floyd[start][end] = cost;
+        floyd[end][start] = cost;
+    }
+    
+    for(let i = 1; i <= n; i++) {
+        for(let j = 1; j <= n; j++) {
+            for(let k = 1; k <= n; k++) {
+                if(floyd[i][j] + floyd[i][k] < floyd[j][k]) {
+                    floyd[j][k] = floyd[i][j] + floyd[i][k];
                 }
             }
         }
     }
     
-    let answer = Number.MAX_SAFE_INTEGER;
+    let answer = Infinity;
     
     for(let i = 1; i <= n; i++) {
         answer = Math.min(answer, floyd[s][i] + floyd[a][i] + floyd[b][i]);
