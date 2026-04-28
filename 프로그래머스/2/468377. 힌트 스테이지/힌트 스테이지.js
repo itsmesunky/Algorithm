e992770238b3879a1de00be3c1250367e7a8de0d
선우 <99164690+itsmesunky@users.noreply.github.com>
2026-04-28 15:17:37 +0900

    [level 2] Title: 힌트 스테이지, Time: 19.36 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/2/468377. 힌트 스테이지/힌트 스테이지.js
+++ 프로그래머스/2/468377. 힌트 스테이지/힌트 스테이지.js
@@ -1,43 +1,50 @@
 const solution = (cost, hint) => {
-    const n = cost.length;
     let answer = Number.MAX_SAFE_INTEGER;
     
-    const dfs = (currStage, currHints, currCost) => {
+    // 총 스테이지 수
+    const stages = cost.length;
+
+    /**
+    * getMinCost - 가능한 조합들을 DFS + 백트래킹으로 모두 탐색
+    * @param {number} currStage - 진행중인 단계(0-indexed)
+    * @param {number}[] currHints - 구매한 힌트들의 남은 개수
+    * @param {number} currCost - 현재까지 사용한 비용
+    */
+    const getMinCost = (currStage, currHints, currCost) => {
         let newCost = 0;
-        if(currHints[currStage]) {
-            const hintCnt = Math.min(currHints[currStage], n - 1);
+        
+        if(currHints[currStage]) { // 현재 스테이지에 해당하는 힌트가 존재하는 경우
+            // ⚠️ 하나의 스테이지에서 사용할 수 있는 힌트권의 최대 개수는 n-1개
+            const hintCnt = Math.min(currHints[currStage], stages - 1);
             newCost = cost[currStage][hintCnt];
             currHints[currStage] = 0;
-        } else {
+        } else { // 현재 스테이지에 해당하는 힌트가 존재하지 않는 경우
             newCost = cost[currStage][0];
         }
         
         currCost += newCost;
         
-        if(currStage === n - 1) {
+        // 현재 스테이지가 마지막 스테이지라면 answer를 더 작은 값으로 갱신 후 종료
+        if(currStage === stages - 1) {
             answer = Math.min(answer, currCost);
             return;
         }
         
+        // 해당 스테이지에서 힌트 번들 구매하는 경우
+        const hints = hint[currStage];
         const copiedHints = [...currHints];
-        
-        for(let i = 0; i < 2; i++) {
-            if(i === 0) { // 해당 스테이지에서 힌트 번들 구매한 경우
-                const hints = hint[currStage];
-                for(let j = 1; j < hints.length; j++) {
-                    copiedHints[hints[j] - 1]++;
-                }
-                dfs(currStage + 1, copiedHints, currCost + hints[0]);
-            } else { // 해당 스테이지에서 힌트 번들 구매하지 않은 경우
-                dfs(currStage + 1, [...currHints], currCost);
-            }
+        for(let i = 1; i < hints.length; i++) {
+            copiedHints[hints[i] - 1]++;
         }
+        getMinCost(currStage + 1, [...copiedHints], currCost + hints[0]);
         
+        // 해당 스테이지에서 힌트 번들 구매하지 않는 경우
+        getMinCost(currStage + 1, currHints, currCost);
+        
+        // 백트래킹
         currCost -= newCost;
     }
     
-    const hints = Array(n).fill(0);
-    dfs(0, [...hints], 0);
-    
+    getMinCost(0, [...Array(stages).fill(0)], 0);
     return answer;
 }
