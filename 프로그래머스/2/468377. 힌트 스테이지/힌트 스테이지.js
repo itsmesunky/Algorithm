f2cd4f1f998eb01eaac71c6ec5e91addd4af8206
선우 <99164690+itsmesunky@users.noreply.github.com>
2026-04-28 15:29:41 +0900

    [level 2] Title: 힌트 스테이지, Time: 10.38 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/468377. 힌트 스테이지/힌트 스테이지.js
+++ 프로그래머스/2/468377. 힌트 스테이지/힌트 스테이지.js
@@ -5,19 +5,20 @@ const solution = (cost, hint) => {
     const stages = cost.length;
 
     /**
-    * getMinCost - 가능한 조합들을 DFS + 백트래킹으로 모두 탐색
+    * getMinCost - 가능한 조합들을 DFS로 탐색
     * @param {number} currStage - 진행중인 단계(0-indexed)
     * @param {number}[] currHints - 구매한 힌트들의 남은 개수
     * @param {number} currCost - 현재까지 사용한 비용
     */
     const getMinCost = (currStage, currHints, currCost) => {
+        if(answer <= currCost) return;
+        
         let newCost = 0;
         
         if(currHints[currStage]) { // 현재 스테이지에 해당하는 힌트가 존재하는 경우
             // ⚠️ 하나의 스테이지에서 사용할 수 있는 힌트권의 최대 개수는 n-1개
             const hintCnt = Math.min(currHints[currStage], stages - 1);
             newCost = cost[currStage][hintCnt];
-            currHints[currStage] = 0;
         } else { // 현재 스테이지에 해당하는 힌트가 존재하지 않는 경우
             newCost = cost[currStage][0];
         }
@@ -40,11 +41,8 @@ const solution = (cost, hint) => {
         
         // 해당 스테이지에서 힌트 번들 구매하지 않는 경우
         getMinCost(currStage + 1, currHints, currCost);
-        
-        // 백트래킹
-        currCost -= newCost;
     }
     
-    getMinCost(0, [...Array(stages).fill(0)], 0);
+    getMinCost(0, Array(stages).fill(0), 0);
     return answer;
 }
